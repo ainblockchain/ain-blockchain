3397c346310573a15447d68279d6bbd5644b61db
Chris <chris@comcom.ai>
2019-04-06 22:18:24 +0900

    updating test script

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
-node /home/chris/VisualStudio/blockchain-database/tracker-server/index.js &
+node $0/../tracker-server/index.js &
 sleep 5
-STAKE=250 LOG=true node server/index.js &
+STAKE=250 LOG=true node $0/../server/index.js &
 sleep 5
-STAKE=250 P2P_PORT=5020 PORT=8081 LOG=true node /home/chris/VisualStudio/blockchain-database/server/index.js &
+STAKE=250 P2P_PORT=5020 PORT=8081 LOG=true node $0/../server/index.js &
 sleep 5
-STAKE=250 P2P_PORT=5019 PORT=8082 LOG=true node /home/chris/VisualStudio/blockchain-database/server/index.js &
+STAKE=250 P2P_PORT=5019 PORT=8082 LOG=true node $0/../server/index.js &
