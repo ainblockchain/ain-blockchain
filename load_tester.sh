e329b7c41f4cb41b6d40cb387ce1005cb1464104
Chris <chris@comcom.ai>
2019-04-22 17:16:56 +0900

    updating load_tester

--- load_tester.sh
+++ load_tester.sh
@@ -4,5 +4,21 @@
 # sudo apt-get install apache2
 
 # Initially focus on improving on improving performance on just one instance
-ab -p post.txt -T application/json  -c 50 -n 10000 http://localhost:8080/set
+node $0/../tracker-server/index.js &
+sleep 5
+STAKE=250 LOG=true node $0/../server/index.js > log1.txt &
+sleep 10
+STAKE=250 P2P_PORT=5020 PORT=8081 LOG=true node $0/../server/index.js > log2.txt &
+sleep 20
+ab -p post.txt -T application/json  -c 50 -n 10000 http://localhost:8080/set > load1.txt &
+sleep 1
+ab -p post.txt -T application/json  -c 50 -n 10000 http://localhost:8080/set > load2.txt 
 
+
+
+sleep 5
+
+wget -O b1.txt http://localhost:8080/blocks
+wget -O b2.txt http://localhost:8081/blocks
+
+diff b1.txt b2.txt
