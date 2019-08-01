369c5f004aa2689d0fa679e5d079f26761f00ef3
chris <https://chrissh@bitbucket.org>
2019-08-01 11:32:41 +0900

    small bug fix for json-rpc block queries

--- blockchain/index.js
+++ blockchain/index.js
@@ -194,20 +194,18 @@ class Blockchain{
     getChainSection(from, to){
         from = Number(from)
         to = to ? Number(to) : this.height()
-        let chain 
+        var chain = []
         if (from < this.chain[0].height){
-            chain = []
             var blockFiles = this.blockFiles()
             var endPoint = to > blockFiles.length ? blockFiles.length: to
             for(var i = from; i < endPoint; i++){
                 chain.push(ForgedBlock.loadBlock(blockFiles[i]))
             }
         } else {
-            chain = this.chain.filter(block => {
-                if (from <= block.height < to){
-                    return block
-                }
-            })
+            var endPoint = to > this.chain.length ? this.chain.length: to
+            for(var i = from; i < endPoint; i++){
+                chain.push(this.chain[i])
+            }
         }
         return chain
     }
