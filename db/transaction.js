7a35cae0b873d41cde8d5a7488f96b6deef24fcf
platfowner <seo@Dongils-iMac.local>
2019-08-19 19:54:48 +0900

    Update rule to check balance. Delete custom address / nonce from operation

--- db/transaction.js
+++ db/transaction.js
@@ -24,15 +24,20 @@ class Transaction {
     let nonce;
     if (data.nonce !== undefined) {
       nonce = data.nonce;
+      delete data.nonce;
     } else if (isNoncedTransaction) {
       nonce = db.nonce;
       db.nonce ++;
     } else {
       nonce = -1;
     }
+    const address = data.address != undefined ? data.address : db.publicKey;
+    const signature = data.address != undefined ? "" : db.sign(ChainUtil.hash(data));
+    if (data.address != undefined) {
+      delete data.address;
+    }
     const transaction =
-        new this(Date.now(), data, data.address ? data.address : db.publicKey,
-                 data.address ? "" : db.sign(ChainUtil.hash(data)), nonce);
+        new this(Date.now(), data, address, signature, nonce);
     return transaction;
   }
 
