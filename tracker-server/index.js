d9958f5a59db76ec9efdb3a7867f950de1647c73
Dongil Seo <seo@comcom.ai>
2019-12-13 20:59:48 +0900

    Change peer handling in tracker server.

--- tracker-server/index.js
+++ tracker-server/index.js
@@ -6,7 +6,7 @@ const express = require('express');
 const jayson = require('jayson');
 
 const MAX_PER_SERVER = 2;
-const PEERS = [];
+const NODES = [];
 const REGION = 'region';
 const COUNTRY = 'country';
 const CITY = 'city';
@@ -15,22 +15,36 @@ const MASK = 'xxx';
 const PORT = 5000;
 
 function printPeers() {
-  for (let i = 0; i < PEERS.length; i++) {
-    const peer = PEERS[i];
-    console.log(`  Peer[${i}]: ${peer.publicKey} (${peer.url})`);
+  console.log(`=> Number of peers: ${NODES.length}`);
+  for (let i = 0; i < NODES.length; i++) {
+    const peer = NODES[i];
+    console.log(`  Peer[${i}]: ${peer.getNodeSummary()})`);
+    for (let j = 0; j < peer.connectedPeers.length; j++) {
+      const connected = peer.connectedPeers[j];
+      console.log(`    Connected[${j}]: ${connected.getNodeSummary()})`);
+    }
   }
 }
 
+// A util function for testing/debugging.
+function setTimer(ws) {
+  setTimeout(() => {
+    ws.close();
+  }, 60000);
+}
+
 webSocketServer.on('connection', (ws) => {
+  /*
+  setTimer(ws);
+  */
   ws.on('message', (message) => {
     try {
       peerUrlInfo = JSON.parse(message);
-      const peer = Peer.getPeer(ws, peerUrlInfo);
+      const peer = Node.getPeer(ws, peerUrlInfo);
       ws.send(JSON.stringify(peer.getPeerList()));
-      console.log(`Added peer node ${peer.url}`);
-      console.log(`New peer node is connected to  ${peer.getPeerList()}`);
-      PEERS.push(peer);
-      console.log(`Number of peers: ${PEERS.length}`);
+      console.log(`Added new peer node ${peer.getNodeSummary()})`);
+      console.log(`New peer node is connected to ${peer.getPeerSummary()}`);
+      NODES.push(peer);
       printPeers();
     } catch (err) {
       console.log(err.stack);
@@ -38,61 +52,76 @@ webSocketServer.on('connection', (ws) => {
   });
 
   ws.on('close', (code) => {
-    console.log(`Connection closed with code: ` + code);
     try {
-      const peer = PEERS.find((p) => p.ws === ws);
-      const peerIndex = PEERS.indexOf(peer);
-      PEERS.splice(peerIndex, 1);
-      console.log(`Number of peers: ${PEERS.length}`);
-      printPeers();
-      const effectedPeers = PEERS.filter((p) => {
+      const peer = NODES.find((p) => p.ws === ws);
+      console.log(`Peer node ${peer.getNodeSummary()}) disconnected with code: ${code}`);
+      const peerIndex = NODES.indexOf(peer);
+      NODES.splice(peerIndex, 1);
+      const effectedPeers = NODES.filter((p) => {
         if (p.getPeerList().indexOf(peer.url) !== -1) {
           return true;
         }
         return false;
       });
-      let lastPeer = effectedPeers.pop();
-      for (let i = effectedPeers.length - 1; i >= 0; i--) {
-        console.log(`Connecting peer ${lastPeer.url} to peer ${effectedPeers[i].url}`);
-        lastPeer.connect(effectedPeers[i]);
-        lastPeer = effectedPeers.pop();
+      for (let i = 0; i < effectedPeers.length; i++) {
+        const effected = effectedPeers[i];
+        effected.removePeer(peer);
+        if (i + 1 < effectedPeers.length) {
+          effected.connect(effectedPeers[i + 1]);
+        }
       }
+      printPeers();
     } catch (err) {
       console.log(err.stack);
     }
   });
 });
 
-class Peer {
+class Node {
   constructor(ws, peerUrlInfo) {
     this.protocol = peerUrlInfo.PROTOCOL;
     this.ip = peerUrlInfo.HOST;
     this.port = peerUrlInfo.P2P_PORT;
     this.publicKey = peerUrlInfo.PUBLIC_KEY;
-    this.url = Peer.getPeerUrl(this.protocol, this.ip, this.port);
+    this.url = Node.getPeerUrl(this.protocol, this.ip, this.port);
     this.ws = ws;
     this.connectedPeers = [];
-    const locationDict = Peer.getPeerLocation(this.ip);
-    this.country = locationDict == null || locationDict[COUNTRY].length === 0 ? null : locationDict[COUNTRY];
-    this.region = locationDict == null ||locationDict[REGION].length === 0 ? null : locationDict[REGION];
-    this.city = locationDict == null ||locationDict[CITY].length === 0 ? null : locationDict[CITY];
-    this.timezone = locationDict == null ||locationDict[TIMEZONE].length === 0 ? null : locationDict[TIMEZONE];
+    const locationDict = Node.getPeerLocation(this.ip);
+    this.country = (locationDict === null || locationDict[COUNTRY].length === 0) ?
+        null : locationDict[COUNTRY];
+    this.region = (locationDict === null ||locationDict[REGION].length === 0) ?
+        null : locationDict[REGION];
+    this.city = (locationDict === null ||locationDict[CITY].length === 0) ?
+        null : locationDict[CITY];
+    this.timezone = (locationDict === null ||locationDict[TIMEZONE].length === 0) ?
+        null : locationDict[TIMEZONE];
   }
 
   getPeerInfo() {
     return {
-      ip: Peer.maskIp(this.ip),
+      ip: Node.maskIp(this.ip),
       port: this.port,
-      url: Peer.getPeerUrl(this.protocol, Peer.maskIp(this.ip), this.port),
+      url: Node.getPeerUrl(this.protocol, Node.maskIp(this.ip), this.port),
       publicKey: this.publicKey,
       connectedPeers: this.connectedPeers.map((peer) => {
-        return Peer.getPeerUrl(peer.protocol, Peer.maskIp(peer.ip), peer.port);
+        return Node.getPeerUrl(peer.protocol, Node.maskIp(peer.ip), peer.port);
       }),
       country: this.country,
       region: this.region,
       city: this.city,
       timezone: this.timezone,
     };
+  }
+
+  getNodeSummary() {
+    return `${this.publicKey.substring(0, 6)} (${this.url})`;
+  }
+
+  getPeerSummary() {
+    const list = this.connectedPeers.map((peer) => {
+      return peer.getNodeSummary();
+    });
+    return list.join(', ');
   }
 
   static maskIp(ip) {
@@ -104,10 +133,17 @@ class Peer {
 
   static getPeerLocation(ip) {
     const geoLocationDict = geoip.lookup(ip);
-    if (geoLocationDict === null || (geoLocationDict[COUNTRY].length === 0 && geoLocationDict[REGION].length === 0 && geoLocationDict[CITY].length === 0 && geoLocationDict[TIMEZONE].length === 0)) {
+    if (geoLocationDict === null || (geoLocationDict[COUNTRY].length === 0 &&
+        geoLocationDict[REGION].length === 0 && geoLocationDict[CITY].length === 0 &&
+        geoLocationDict[TIMEZONE].length === 0)) {
       return null;
     }
-    return {[COUNTRY]: geoLocationDict[COUNTRY], [REGION]: geoLocationDict[REGION], [CITY]: geoLocationDict[CITY], [TIMEZONE]: geoLocationDict[TIMEZONE]};
+    return {
+      [COUNTRY]: geoLocationDict[COUNTRY],
+      [REGION]: geoLocationDict[REGION],
+      [CITY]: geoLocationDict[CITY],
+      [TIMEZONE]: geoLocationDict[TIMEZONE]
+    };
   }
 
   static getPeerUrl(protocol, host, port) {
@@ -115,12 +151,12 @@ class Peer {
   }
 
   static getPeer(ws, peerInfo) {
-    const peer = new Peer(ws, peerInfo);
-    if (PEERS.length == 1) {
-      peer.addPeer(PEERS[0]);
-    } else if (PEERS.length > 1) {
+    const peer = new Node(ws, peerInfo);
+    if (NODES.length === 1) {
+      peer.addPeer(NODES[0]);
+    } else if (NODES.length > 1) {
       while (peer.getPeerList().length < MAX_PER_SERVER) {
-        peer.addPeer(PEERS[Math.floor(Math.random() * PEERS.length)]);
+        peer.addPeer(NODES[Math.floor(Math.random() * NODES.length)]);
       }
     }
 
@@ -148,7 +184,9 @@ class Peer {
         return p;
       }
     });
-    peer.removePeer(this);
+    if (peer) {
+      peer.removePeer(this);
+    }
   }
 
   getPeerList() {
@@ -159,6 +197,7 @@ class Peer {
   }
 
   connect(peer) {
+    console.log(`Connecting ${this.getNodeSummary()}) to ${peer.getNodeSummary()})`);
     this.ws.send(JSON.stringify([peer.url]));
     this.addPeer(peer);
   }
@@ -166,7 +205,7 @@ class Peer {
 
 const app = express();
 app.use(express.json()); // support json encoded bodies
-const jsonRpcMethods = require('./json-rpc')(PEERS);
+const jsonRpcMethods = require('./json-rpc')(NODES);
 app.post('/json-rpc', jayson.server(jsonRpcMethods).middleware());
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
