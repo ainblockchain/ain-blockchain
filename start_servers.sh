d6931056b449605c2064c242d1b626ad990a3181
Lia Yoo <liayoo@Lias-iMac.local>
2020-01-28 12:45:57 +0900

    Update start_servers.sh and npm script.

--- start_servers.sh
+++ start_servers.sh
@@ -1,14 +1,12 @@
 node $0/../tracker-server/index.js &
 sleep 5
-STAKE=250 P2P_PORT=5001 PORT=8081 ACCOUNT_INDEX=0 LOG=true LOCAL=true node $0/../client/index.js &
+STAKE=250 P2P_PORT=5001 PORT=8081 ACCOUNT_INDEX=0 LOG=true HOSTING_ENV=local node $0/../client/index.js &
 sleep 10
-STAKE=250 P2P_PORT=5002 PORT=8082 ACCOUNT_INDEX=1 LOG=true LOCAL=true node $0/../client/index.js &
+STAKE=250 P2P_PORT=5002 PORT=8082 ACCOUNT_INDEX=1 LOG=true HOSTING_ENV=local node $0/../client/index.js &
 sleep 10
-STAKE=250 P2P_PORT=5003 PORT=8083 ACCOUNT_INDEX=2 LOG=true LOCAL=true node $0/../client/index.js &
+STAKE=250 P2P_PORT=5003 PORT=8083 ACCOUNT_INDEX=2 LOG=true HOSTING_ENV=local node $0/../client/index.js &
 sleep 10
-STAKE=250 P2P_PORT=5004 PORT=8084 ACCOUNT_INDEX=3 LOG=true LOCAL=true node $0/../client/index.js &
+STAKE=250 P2P_PORT=5004 PORT=8084 ACCOUNT_INDEX=3 LOG=true HOSTING_ENV=local node $0/../client/index.js &
 sleep 10
 # STAKE=250 P2P_PORT=5005 PORT=8085 ACCOUNT_INDEX=4 LOG=true LOCAL=true node $0/../client/index.js &
 # sleep 10
-
-
