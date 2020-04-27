9a1f07b543054e6630d9c9262f566d2c69e01546
Dongil Seo <seo@comcom.ai>
2020-04-27 15:48:12 +0900

    Update deploy scripts for new dev cluster.

--- deploy_gcp.sh
+++ deploy_gcp.sh
@@ -5,11 +5,8 @@ if [ "$#" -lt 2 ]; then
     exit
 fi
 
-if [ "$1" = 'spring' ] || [ "$1" = 'summer' ]; then
-    SEASON="$1-"
-# TODO(lia): Change dev instance names to start with 'dev-' and update this script
-elif [ "$1" = 'dev' ]; then
-    SEASON=""
+if [ "$1" = 'spring' ] || [ "$1" = 'summer' ] || [ "$1" = 'dev' ]; then
+    SEASON="$1"
 else
     echo "Invalid project/season argument: $1"
     exit
@@ -25,26 +22,26 @@ FILES_FOR_NODE="blockchain/ chain-util.js client/ consensus/ constants.js db jso
 printf "\nRemoving redundant files..."
 rm -rf blockchain/blockchains logger/logs tracker-server/node_modules tracker-server/logs
 
-TRACKER_TARGET_ADDR="${GCP_USER}@${SEASON}tracker-taiwan"
+TRACKER_TARGET_ADDR="${GCP_USER}@${SEASON}-tracker-taiwan"
 printf "\nDeploying files to ${TRACKER_TARGET_ADDR}..."
 gcloud compute scp --recurse $FILES_FOR_TRACKER ${TRACKER_TARGET_ADDR}:~/
 
-NODE_0_TARGET_ADDR="${GCP_USER}@${SEASON}node-0-taiwan"
+NODE_0_TARGET_ADDR="${GCP_USER}@${SEASON}-node-0-taiwan"
 printf "\nDeploying files to ${NODE_0_TARGET_ADDR}..."
 gcloud compute scp --recurse $FILES_FOR_NODE ${NODE_0_TARGET_ADDR}:~/
 
-NODE_1_TARGET_ADDR="${GCP_USER}@${SEASON}node-1-oregon"
+NODE_1_TARGET_ADDR="${GCP_USER}@${SEASON}-node-1-oregon"
 printf "\nDeploying files to ${NODE_1_TARGET_ADDR}..."
 gcloud compute scp --recurse $FILES_FOR_NODE ${NODE_1_TARGET_ADDR}:~/
 
-NODE_2_TARGET_ADDR="${GCP_USER}@${SEASON}node-2-singapore"
+NODE_2_TARGET_ADDR="${GCP_USER}@${SEASON}-node-2-singapore"
 printf "\nDeploying files to ${NODE_2_TARGET_ADDR}..."
 gcloud compute scp --recurse $FILES_FOR_NODE ${NODE_2_TARGET_ADDR}:~/
 
-NODE_3_TARGET_ADDR="${GCP_USER}@${SEASON}node-3-iowa"
+NODE_3_TARGET_ADDR="${GCP_USER}@${SEASON}-node-3-iowa"
 printf "\nDeploying files to ${NODE_3_TARGET_ADDR}..."
 gcloud compute scp --recurse $FILES_FOR_NODE ${NODE_3_TARGET_ADDR}:~/
 
-NODE_4_TARGET_ADDR="${GCP_USER}@${SEASON}node-4-netherlands"
+NODE_4_TARGET_ADDR="${GCP_USER}@${SEASON}-node-4-netherlands"
 printf "\nDeploying files to ${NODE_4_TARGET_ADDR}..."
 gcloud compute scp --recurse $FILES_FOR_NODE ${NODE_4_TARGET_ADDR}:~/
