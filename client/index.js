ab4ba5b5edbd3182c68c1a0328655b4e1b7fadab
platfowner <seo@comcom.ai>
2020-06-15 15:48:53 +0900

    Set express keep alive timeout.

--- client/index.js
+++ client/index.js
@@ -278,10 +278,13 @@ app.get('/get_address', (req, res, next) => {
 
 // We will want changes in ports and the database to be broadcast across
 // all instances so lets pass this info into the p2p server
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   logger.info(`App listening on port ${PORT}`);
   logger.info('Press Ctrl+C to quit.');
 });
+
+server.keepAliveTimeout = 620 * 1000; // 620 seconds
+server.headersTimeout = 630 * 1000; // 630 seconds
 
 // Lets start this p2p server up so we listen for changes in either DATABASE
 // or NUMBER OF SERVERS
--- tracker-server/index.js
+++ tracker-server/index.js
@@ -297,7 +297,10 @@ app.get('/peer_nodes', (req, res, next) => {
     .end();
 });
 
-app.listen(PORT, () => {
+const trackerServer = app.listen(PORT, () => {
   logger.info(`App listening on port ${PORT}`);
   logger.info('Press Ctrl+C to quit.');
 });
+
+trackerServer.keepAliveTimeout = 620 * 1000; // 620 seconds
+trackerServer.headersTimeout = 630 * 1000; // 630 seconds
