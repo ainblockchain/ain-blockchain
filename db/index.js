c7d4174d807a287b576aeda99280e18e2a5b3f09
Lia Yoo <lia@comcom.ai>
2020-07-29 21:59:59 +0900

    Remove an unnecessary condition.

--- db/index.js
+++ db/index.js
@@ -791,11 +791,7 @@ class DB {
   }
 
   lastBlockNumber() {
-    if (!!this.bc && !!this.bc.lastBlockNumber) {
-      return this.bc.lastBlockNumber();
-    } else {
-      return this.blockNumberSnapshot;
-    }
+    return !!this.bc ? this.bc.lastBlockNumber() : this.blockNumberSnapshot;
   }
 
   static hasOwnerConfig(ownerNode) {
