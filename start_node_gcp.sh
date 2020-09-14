9a19aaedd87841051f14a24b34e556e9c34d0e19
Lia Yoo <lia@comcom.ai>
2020-09-14 21:01:05 +0900

    Update scripts and configs for shards.

--- start_node_gcp.sh
+++ start_node_gcp.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ "$#" -lt 2 ]; then
-    echo "Usage: sh start_node.sh spring 0"
+    echo "Usage: sh start_node.sh spring 0 0"
     exit
 fi
 
@@ -9,23 +9,38 @@ if [ "$1" = 'spring' ]; then
     export TRACKER_WS_ADDR=ws://35.221.137.80:5000  # spring-tracker-ip
 elif [ "$1" = 'summer' ]; then
     export TRACKER_WS_ADDR=ws://35.194.172.106:5000  # summer-tracker-ip
-elif [ "$1" = 'dev' ]; then
-    export TRACKER_WS_ADDR=ws://34.80.184.73:5000  # dev-tracker-ip
 elif [ "$1" = 'staging' ]; then
     export TRACKER_WS_ADDR=ws://35.221.150.73:5000 # staging-tracker-ip
+elif [ "$1" = 'dev' ]; then
+  if [ "$2" = 0 ]; then
+    export TRACKER_WS_ADDR=ws://34.80.184.73:5000  # dev-tracker-ip
+  elif [ "$2" = 1 ]; then
+    export TRACKER_WS_ADDR=ws://35.187.153.22:5000  # dev-shard-1-tracker-ip
+  elif [ "$2" = 2 ]; then
+    export TRACKER_WS_ADDR=ws://34.80.203.104:5000  # dev-shard-2-tracker-ip
+  else
+    echo "Invalid shard ID argument: $2"
+    exit
+  fi
 else
     echo "Invalid season argument: $1"
     exit
 fi
 echo "TRACKER_WS_ADDR=$TRACKER_WS_ADDR"
 
-if [ "$2" -lt 0 ] || [ "$2" -gt 4 ]; then
+if [ "$3" -lt 0 ] || [ "$3" -gt 4 ]; then
     echo "Invalid account_index argument: $2"
     exit
 fi
 
-export NUM_VALIDATORS=5
-export ACCOUNT_INDEX="$2"
+if [ "$2" = 0 ]; then
+  export NUM_VALIDATORS=5
+else
+  export NUM_VALIDATORS=3
+  export GENESIS_CONFIGS_DIR="blockchain/shard_$2"
+fi
+
+export ACCOUNT_INDEX="$3"
 echo "ACCOUNT_INDEX=$ACCOUNT_INDEX"
 
 export HOSTING_ENV=gcp
