e6401454f4e301fbf2c2de7c3c63591b3f37710c
minsulee2 <minsu@comcom.ai>
2020-09-15 15:46:33 +0900

    Set color as well

--- logger/index.js
+++ logger/index.js
@@ -1,10 +1,12 @@
 const winston = require('winston');
-const { getWinstonLevels, getWinstonTransports } = require('./winston-util');
+const { getWinstonLevels, getWinstonColors, getWinstonTransports } = require('./winston-util');
 
 const logger = new winston.createLogger({
   levels: getWinstonLevels(),
   transports: getWinstonTransports(),
   exitOnError: false
 });
+
+winston.addColors(getWinstonColors());
 
 module.exports = logger;
