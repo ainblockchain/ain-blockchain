999855e501385891145556f3221723024eee3ccc
minsulee2 <minsu@comcom.ai>
2020-09-15 11:46:11 +0900

    Make winston-util.js

--- logger/index.js
+++ logger/index.js
@@ -1,63 +1,17 @@
-const process = require('process');
 const winston = require('winston');
-const winstonDaily = require('winston-daily-rotate-file');
-const path = require('path');
 const { LoggingWinston } = require('@google-cloud/logging-winston');
-const { PORT, ACCOUNT_INDEX, HOSTING_ENV } = require('../constants');
-
-const { combine, timestamp, label, printf } = winston.format
-const logFormat = printf(({ level, message, label, timestamp }) => {
-  return `${timestamp} [${label}] ${level}: ${message}`;
-})
-
-const logDir = path.join(__dirname, '.', 'logs', String(PORT));
-const prefix = `node-${ACCOUNT_INDEX}`;
+const { HOSTING_ENV } = require('../constants');
+const {
+  getWinstonDebugConsoleTransport,
+  getWinstonDailyDebugFileTransport,
+  getWinstonDailyErrorFileTransport,
+} = require('./winston-util');
 
 function getTransports() {
   const transports = [
-    new (winstonDaily)({
-      name: 'daily-combined-log',
-      // level: 'info',
-      level: 'debug',
-      filename: `${logDir}/${prefix}-combined-%DATE%.log`,
-      handleExceptions: true,
-      json: false,
-      maxSize: '100m',
-      maxFiles: '14d',
-      colorize: false,
-      format: combine(
-        label({ label: prefix }),
-        timestamp(),
-        logFormat
-      )
-    }),
-    new (winstonDaily)({
-      name: 'daily-error-log',
-      level: 'error',
-      filename: `${logDir}/${prefix}-error-%DATE%.log`,
-      handleExceptions: true,
-      json: false,
-      maxSize: '100m',
-      maxFiles: '180d',
-      colorize: false,
-      format: combine(
-        label({ label: prefix }),
-        timestamp(),
-        logFormat
-      )
-    }),
-    new (winston.transports.Console)({
-      name: 'debug-console-log',
-      level: 'debug',
-      handleExceptions: true,
-      json: false,
-      colorize: true,
-      format: combine(
-        label({ label: prefix }),
-        timestamp(),
-        logFormat
-      )
-    }),
+    getWinstonDebugConsoleTransport(),
+    getWinstonDailyDebugFileTransport(),
+    getWinstonDailyErrorFileTransport(),
   ];
   if (HOSTING_ENV === 'gcp') {
     // Add Stackdriver Logging
---
+++ logger/winston-util.js
@@ -0,0 +1,69 @@
+const winston = require('winston');
+const winstonDaily = require('winston-daily-rotate-file');
+const path = require('path');
+const { PORT, ACCOUNT_INDEX } = require('../constants');
+
+const { combine, timestamp, label, printf } = winston.format;
+
+const logDir = path.join(__dirname, '.', 'logs', String(PORT));
+const prefix = `node-${ACCOUNT_INDEX}`;
+const logFormat = printf(({ level, message, label, timestamp }) => {
+  return `${timestamp} [${label}] ${level}: ${message}`;
+});
+
+const getWinstonDailyDebugFileTransport = () => {
+  return new (winstonDaily) ({
+    name: 'daily-combined-log',
+    level: 'debug',
+    filename: `${logDir}/${prefix}-combined-%DATE%.log`,
+    handleExceptions: true,
+    json: false,
+    maxSize: '100m',
+    maxFiles: '14d',
+    colorize: false,
+    format: combine(
+      label({ label: prefix }),
+      timestamp(),
+      logFormat
+    )
+  });
+};
+
+const getWinstonDebugConsoleTransport = () => {
+  return new (winston.transports.Console) ({
+    name: 'debug-console-log',
+    level: 'debug',
+    handleExceptions: true,
+    json: false,
+    colorize: true,
+    format: combine(
+      label({ label: prefix }),
+      timestamp(),
+      logFormat
+    )
+  });
+};
+
+const getWinstonDailyErrorFileTransport = () => {
+  return new (winstonDaily) ({
+    name: 'daily-error-log',
+    level: 'error',
+    filename: `${logDir}/${prefix}-error-%DATE%.log`,
+    handleExceptions: true,
+    json: false,
+    maxSize: '100m',
+    maxFiles: '180d',
+    colorize: false,
+    format: combine(
+      label({ label: prefix }),
+      timestamp(),
+      logFormat
+    )
+  });
+};
+
+module.exports = {
+  getWinstonDebugConsoleTransport,
+  getWinstonDailyDebugFileTransport,
+  getWinstonDailyErrorFileTransport,
+};
