7f17f58354334afcd2317227dcd46604c7decb81
Lia Yoo <lia@comcom.ai>
2020-09-17 13:55:02 +0900

    Add scripts for shard clusters.

--- setup_node_gcp.sh
+++ setup_node_gcp.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 cd
+killall node
 sudo rm -rf ../ain-blockchain
 sudo mkdir ../ain-blockchain
 sudo chmod 777 ../ain-blockchain
 mv * ../ain-blockchain
 cd ../ain-blockchain
 npm install
-# TODO(lia): create blockchain/shard_{i}/genesis_* files if needed
