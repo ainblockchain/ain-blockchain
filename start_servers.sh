6f4e111e141a4c1629913201686e560ddce1d9e2
Lia Yoo <lia@comcom.ai>
2020-09-22 10:49:47 +0900

    Update start_servers.sh.

--- start_servers.sh
+++ start_servers.sh
@@ -1,10 +1,29 @@
-node $0/../tracker-server/index.js &
+# PARENT CHAIN
+node ./tracker-server/index.js &
 sleep 5
-NUM_VALIDATORS=4 ACCOUNT_INDEX=0 HOSTING_ENV=local node $0/../client/index.js &
+NUM_VALIDATORS=3 ACCOUNT_INDEX=0 HOSTING_ENV=local node ./client/index.js &
 sleep 10
-NUM_VALIDATORS=4 ACCOUNT_INDEX=1 HOSTING_ENV=local node $0/../client/index.js &
+NUM_VALIDATORS=3 ACCOUNT_INDEX=1 HOSTING_ENV=local node ./client/index.js &
 sleep 10
-NUM_VALIDATORS=4 ACCOUNT_INDEX=2 HOSTING_ENV=local node $0/../client/index.js &
+NUM_VALIDATORS=3 ACCOUNT_INDEX=2 HOSTING_ENV=local node ./client/index.js &
 sleep 10
-NUM_VALIDATORS=4 ACCOUNT_INDEX=3 HOSTING_ENV=local node $0/../client/index.js &
+
+# CHILD CHAIN 1
+PORT=9010 P2P_PORT=6010 node ./tracker-server/index.js &
+sleep 10
+PORT=9011 P2P_PORT=6011 TRACKER_WS_ADDR=ws://localhost:6010 NUM_VALIDATORS=3 ACCOUNT_INDEX=0 HOSTING_ENV=local GENESIS_CONFIGS_DIR=blockchain/shard_1 node ./client/index.js &
+sleep 10
+PORT=9012 P2P_PORT=6012 TRACKER_WS_ADDR=ws://localhost:6010 NUM_VALIDATORS=3 ACCOUNT_INDEX=1 HOSTING_ENV=local GENESIS_CONFIGS_DIR=blockchain/shard_1 node ./client/index.js &
+sleep 10
+PORT=9013 P2P_PORT=6013 TRACKER_WS_ADDR=ws://localhost:6010 NUM_VALIDATORS=3 ACCOUNT_INDEX=2 HOSTING_ENV=local GENESIS_CONFIGS_DIR=blockchain/shard_1 node ./client/index.js &
+sleep 10
+
+# CHILD CHAIN 2
+PORT=9020 P2P_PORT=6020 node ./tracker-server/index.js
+sleep 10
+PORT=9021 P2P_PORT=6021 TRACKER_WS_ADDR=ws://localhost:6020 NUM_VALIDATORS=3 ACCOUNT_INDEX=0 HOSTING_ENV=local GENESIS_CONFIGS_DIR=blockchain/shard_2 node ./client/index.js &
+sleep 1000
+PORT=9022 P2P_PORT=6022 TRACKER_WS_ADDR=ws://localhost:6020 NUM_VALIDATORS=3 ACCOUNT_INDEX=1 HOSTING_ENV=local GENESIS_CONFIGS_DIR=blockchain/shard_2 node ./client/index.js &
+sleep 10
+PORT=9023 P2P_PORT=6023 TRACKER_WS_ADDR=ws://localhost:6020 NUM_VALIDATORS=3 ACCOUNT_INDEX=2 HOSTING_ENV=local GENESIS_CONFIGS_DIR=blockchain/shard_2 node ./client/index.js &
 sleep 10
