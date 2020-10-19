c321eaef7d27692644c1c92da4d500223b8db1f8
Dongil Seo <seo@comcom.ai>
2020-10-19 19:49:19 +0900

    Add tool scripts for cross-shard debugging.

---
+++ tools/cross-shard/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  nodeUrl: "http://localhost:8081",
+  address: "0x09A0d53FDf1c36A131938eb379b98910e55EEfe1",
+  privateKey: "ee0b1315d446e5318eb6eb4e9d071cd12ef42d2956d546f9acbdc3b75c469640",
+  targetPath: "/apps/afan",
+};
