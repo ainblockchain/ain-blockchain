9c88ba4835f064edc30b5818feffb31d5e5344f0
Dongil Seo <seo@comcom.ai>
2020-10-21 13:42:38 +0900

    Add transaction confirmation logic to sendCheckinTx.js.

--- tools/cross-shard/sendCheckinTx.js
+++ tools/cross-shard/sendCheckinTx.js
@@ -1,5 +1,6 @@
 const _ = require("lodash");
 const axios = require('axios');
+const { sleep } = require('sleep');
 const ainUtil = require('@ainblockchain/ain-util');
 const config = require("./config");
 const ChainUtil = require('../../chain-util');
@@ -33,7 +34,9 @@ function buildTriggerTx(address, payload, timestamp) {
   }
 }
 
-function signTx(tx, keyBuffer) {
+function signTx(tx, privateKey) {
+  console.log(`\n*** signTx():`);
+  const keyBuffer = Buffer.from(privateKey, 'hex');
   const sig = ainUtil.ecSignTransaction(tx, keyBuffer);
   const sigBuffer = ainUtil.toBuffer(sig);
   const lenHash = sigBuffer.length - 65;
@@ -49,10 +52,11 @@ function signTx(tx, keyBuffer) {
   };
 }
 
-function signAndSendTx(endpoint, txBody, keyBuffer) {
-  const { txHash, signedTx } = signTx(txBody, keyBuffer);
+function signAndSendTx(endpointUrl, txBody, privateKey) {
+  console.log(`\n*** signAndSendTx():`);
+  const { txHash, signedTx } = signTx(txBody, privateKey);
   return axios.post(
-      endpoint,
+      `${endpointUrl}/json-rpc`,
       {
         method: "ain_sendSignedTransaction",
         params: signedTx,
@@ -73,29 +77,71 @@ function signAndSendTx(endpoint, txBody, keyBuffer) {
   });
 }
 
+async function sendGetTxByHashRequest(endpointUrl, txHash) {
+  return await axios.post(
+      `${endpointUrl}/json-rpc`,
+      {
+        method: 'ain_getTransactionByHash',
+        params: {
+          protoVer: CURRENT_PROTOCOL_VERSION,
+          hash: txHash,
+        },
+        jsonrpc: "2.0",
+        id: 0
+      })
+  .then(function (resp) {
+    return _.get(resp, 'data.result.result');
+  });
+}
+
 async function sendTransaction() {
-  console.log(`config: ${JSON.stringify(config, null, 2)}`);
-  const endpoint = `${config.endpointUrl}/json-rpc`;
-  const keyBuffer = Buffer.from(config.userPrivateKey, 'hex');
+  console.log(`\n*** sendTransaction():`);
   const timestamp = Date.now();
   const payloadTxBody =
       buildPayloadTx(config.userAddr, config.shardOwnerAddr, config.parentTokenAmount, timestamp);
   console.log(`payloadTxBody: ${JSON.stringify(payloadTxBody, null, 2)}`);
-  const signedPayloadTx = signTx(payloadTxBody, keyBuffer);
+  const signedPayloadTx = signTx(payloadTxBody, config.userPrivateKey);
   console.log(`signedPayloadTx: ${JSON.stringify(signedPayloadTx, null, 2)}`);
+
   const triggerTxBody =
       buildTriggerTx(config.userAddr, signedPayloadTx.signedTx, timestamp);
   console.log(`triggerTxBody: ${JSON.stringify(triggerTxBody, null, 2)}`);
+
   console.log('Sending job transaction...')
-  const txInfo = await signAndSendTx(endpoint, triggerTxBody, keyBuffer);
+  const txInfo = await signAndSendTx(config.endpointUrl, triggerTxBody, config.userPrivateKey);
   console.log(`txInfo: ${JSON.stringify(txInfo, null, 2)}`);
+  return { timestamp, txInfo };
 }
 
-function processArguments() {
+async function confirmTransaction(timestamp, txHash) {
+  console.log(`\n*** confirmTransaction():`);
+  console.log(`txHash: ${txHash}`);
+  let iteration = 0;
+  let result = null;
+  while(true) {
+    iteration++;
+    result = await sendGetTxByHashRequest(config.endpointUrl, txHash);
+    sleep(1);
+    if (result.is_confirmed) {
+      break;
+    }
+  }
+  console.log(`iteration = ${iteration}, result: ${JSON.stringify(result, null, 2)}`);
+  console.log(`elapsed time (ms) = ${result.confirmed_at - timestamp}`);
+}
+
+async function sendCheckinTransaction() {
+  console.log(`\n*** sendTransaction():`);
+  console.log(`config: ${JSON.stringify(config, null, 2)}`);
+  const { timestamp, txInfo } = await sendTransaction();
+  await confirmTransaction(timestamp, txInfo.txHash);
+}
+
+async function processArguments() {
   if (process.argv.length !== 2) {
     usage();
   }
-  return sendTransaction();
+  await sendCheckinTransaction();
 }
 
 function usage() {
