62506249f7f38798a42dd799e37f1e19f6b8a4ab
Lia Yoo <lia@comcom.ai>
2020-10-26 15:29:26 +0900

    Set is_global to true for checkin related txs.

--- tools/cross-shard/config_gcp.js
+++ tools/cross-shard/config_gcp.js
@@ -1,8 +1,10 @@
+// Parent chain <=> Shard 1 (GCP)
 module.exports = {
-  endpointUrl: "http://localhost:9011",
+  endpointUrl: "http://35.197.153.179:8080",
   userAddr: "0x09A0d53FDf1c36A131938eb379b98910e55EEfe1",
   userPrivateKey: "ee0b1315d446e5318eb6eb4e9d071cd12ef42d2956d546f9acbdc3b75c469640",
-  shardOwnerAddr: "0xBBB2219cD5eACc54Ce95deF7a67dDe71C8241891",
+  shardOwnerAddr: "0xB0B0d133E6a82A982396Fa7E4B4F9eC85d1DFBF4",
+  shardingPath: "/apps/afan",
   parentTokenAmount: 1,  // Parent token amount for checkin
   shardTokenAmount: 10,  // Shard token amount for checkout
 };
