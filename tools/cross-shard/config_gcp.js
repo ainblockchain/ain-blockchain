70aced6f148f1916026b284cdd1995567b77daa3
Lia Yoo <lia@comcom.ai>
2020-10-26 19:02:05 +0900

    Update shardingPath in config_gcp.js.

--- tools/cross-shard/config_gcp.js
+++ tools/cross-shard/config_gcp.js
@@ -4,7 +4,7 @@ module.exports = {
   userAddr: "0x09A0d53FDf1c36A131938eb379b98910e55EEfe1",
   userPrivateKey: "ee0b1315d446e5318eb6eb4e9d071cd12ef42d2956d546f9acbdc3b75c469640",
   shardOwnerAddr: "0xB0B0d133E6a82A982396Fa7E4B4F9eC85d1DFBF4",
-  shardingPath: "/apps/afan",
+  shardingPath: "/apps/shard_1",
   parentTokenAmount: 1,  // Parent token amount for checkin
   shardTokenAmount: 10,  // Shard token amount for checkout
 };
