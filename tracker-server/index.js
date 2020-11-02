3528195192123aaeca97a18854c3d7c9d3f49792
minsulee2 <minsu@comcom.ai>
2020-11-02 14:29:40 +0900

    Remove duplicated logger file

--- tracker-server/index.js
+++ tracker-server/index.js
@@ -4,7 +4,7 @@ const geoip = require('geoip-lite');
 const express = require('express');
 const jayson = require('jayson');
 const _ = require('lodash');
-const logger = require('./logger');
+const logger = require('../logger');
 
 const P2P_PORT = process.env.P2P_PORT || 5000;
 const PORT = process.env.PORT || 8080;
