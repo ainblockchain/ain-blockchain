be6915c47109d2dfe7486e7d1f4accdb2495c8b1
minsulee2 <minsu@comcom.ai>
2020-11-02 11:57:10 +0900

    Lint at afan_client/

--- afan_client/request_manager.js
+++ afan_client/request_manager.js
@@ -3,32 +3,32 @@ const Ref = require('./ref');
 const rp = require('request-promise');
 
 class RequestManager {
-  constructor(endpoint, root) {
+  constructor (endpoint, root) {
     this.endpoint = endpoint;
     this.root = root;
     this.updates = [];
   }
 
-  send() {
+  send () {
     const options = {
       method: 'POST',
       uri: this.endpoint + '/set',
-      body: {op_list: this.updates},
+      body: { op_list: this.updates },
       json: true, // Automatically stringifies the body to JSON
     };
 
     return rp(options)
-        .then(function(parsedBody) {
-          // POST succeeded
-          return parsedBody;
-        })
-        .catch(function(err) {
-          // POST failed
-          console.log(err);
-        });
+      .then(function (parsedBody) {
+        // POST succeeded
+        return parsedBody;
+      })
+      .catch(function (err) {
+        // POST failed
+        console.log(err);
+      });
   }
 
-  getRef(ref) {
+  getRef (ref) {
     const options = {
       uri: this.endpoint + `/get_value?ref=${this.root}/${ref}`,
       json: true,
@@ -36,72 +36,72 @@ class RequestManager {
     return rp(options);
   }
 
-  getInvestors(uid) {
+  getInvestors (uid) {
     return this.getRef(Ref.investors(uid));
   }
 
-  getAdState(from, to) {
+  getAdState (from, to) {
     return this.getRef(Ref.adagency_state(from, to));
   }
 
-  update(ref, value) {
-    this.updates.push({type: 'SET_VALUE', ref: this.root + '/' + ref, value});
+  update (ref, value) {
+    this.updates.push({ type: 'SET_VALUE', ref: this.root + '/' + ref, value });
   }
 
-  setAdState(from, to) {
+  setAdState (from, to) {
     this.update(Ref.adagency_state(from, to), 0);
     this.update(Ref.advertisers_state(to, from), 0);
   }
 
-  increase(ref, value) {
+  increase (ref, value) {
     if (value > 0) {
-      this.updates.push({type: 'INC_VALUE', ref: this.root + '/' + ref, value});
+      this.updates.push({ type: 'INC_VALUE', ref: this.root + '/' + ref, value });
     } else {
-      this.updates.push({type: 'DEC_VALUE', ref: this.root + '/' + ref, value: value * -1});
+      this.updates.push({ type: 'DEC_VALUE', ref: this.root + '/' + ref, value: value * -1 });
     }
   }
 
-  increaseBalance(a, value) {
+  increaseBalance (a, value) {
     this.increase(Ref.balance(a), value);
   }
 
-  increaseActivityProfit(to, value) {
+  increaseActivityProfit (to, value) {
     this.increase(Ref.activity_profit(to), value);
   }
 
-  increaseInvestProfit(to, value) {
+  increaseInvestProfit (to, value) {
     this.increase(Ref.invest_profit(to), value);
   }
 
-  increaseAdsProfit(to, value) {
+  increaseAdsProfit (to, value) {
     this.increase(Ref.ads_profit(to), value);
   }
 
-  increaseInvestTotal(to, value) {
+  increaseInvestTotal (to, value) {
     this.increase(Ref.invest_total(to), value);
   }
 
-  increaseInvestNetTotal(to, value) {
+  increaseInvestNetTotal (to, value) {
     this.increase(Ref.invest_net_total(to), value);
   }
 
-  increaseInvestorBalance(to, from, value) {
+  increaseInvestorBalance (to, from, value) {
     this.increase(Ref.investors_amount(to, from), value);
   }
 
-  increasePostCrushOn(to, puid, from, value) {
+  increasePostCrushOn (to, puid, from, value) {
     this.increase(Ref.user_posts_crushOns(to, puid, from), value);
   }
 
-  increaseReplyCrushOn(puid, rid, from, value) {
+  increaseReplyCrushOn (puid, rid, from, value) {
     this.increase(Ref.replies_crushOns(puid, rid, from), value);
   }
 
-  increasePortfolioProfit(to, from, value) {
+  increasePortfolioProfit (to, from, value) {
     this.increase(Ref.portfolio_profit(to, from), value);
   }
 
-  increasePortfolioInvest(from, to, value) {
+  increasePortfolioInvest (from, to, value) {
     this.increase(Ref.portfolio_invest(from, to), value);
   }
 }
