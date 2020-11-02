bfa3ee4037c96f8905725f28ede9bd1fd45d7742
Hanjeong Lee <hanjeong@comcom.ai>
2020-11-02 19:50:09 +0900

    add semicolon (#133)

--- tracker-server/logger.js
+++ tracker-server/logger.js
@@ -74,17 +74,17 @@ const winstonLogger = new winston.createLogger({
 const logger = function(prefix) {
   const prefixedLogger = {
     error: function(text) {
-      winstonLogger.error(`[${prefix}] ${text}`)
+      winstonLogger.error(`[${prefix}] ${text}`);
     },
     info: function(text) {
-      winstonLogger.info(`[${prefix}] ${text}`)
+      winstonLogger.info(`[${prefix}] ${text}`);
     },
     debug: function(text) {
-      winstonLogger.debug(`[${prefix}] ${text}`)
+      winstonLogger.debug(`[${prefix}] ${text}`);
     }
   }
 
-  return prefixedLogger
+  return prefixedLogger;
 }
 
 module.exports = logger;
