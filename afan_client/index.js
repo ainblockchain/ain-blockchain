1400c9cdd0de4bb3e2b77797b12e45957d947be8
minsulee2 <minsu@comcom.ai>
2020-11-02 11:57:10 +0900

    Lint at afan_client/

--- afan_client/index.js
+++ afan_client/index.js
@@ -7,11 +7,11 @@ const INVEST_FEE = 0.1;
 const APP_NAME = 'afan';
 
 class AfanClient {
-  constructor(endpoint) {
+  constructor (endpoint) {
     this.endpoint = endpoint;
   }
 
-  tx_invest(from, to, value) {
+  tx_invest (from, to, value) {
     const requestManager = new RequestManager(this.endpoint, APP_NAME);
     requestManager.increaseBalance(from, -value);
     requestManager.increaseInvestorBalance(to, from, value);
@@ -24,13 +24,13 @@ class AfanClient {
     return requestManager.send();
   }
 
-  async shareProfit(requestManager, from, to, value) {
+  async shareProfit (requestManager, from, to, value) {
     let investors;
     try {
       const investorsResponse = await requestManager.getInvestors(to);
       investors = investorsResponse.result;
     } catch (err) {
-      if (err instanceof errors.StatusCodeError){
+      if (err instanceof errors.StatusCodeError) {
         investors = null;
       } else {
         throw err
@@ -38,12 +38,11 @@ class AfanClient {
     }
 
     requestManager.increaseBalance(from, -value);
-    const profitManager = new ProfitManager(to, from, investors, requestManager,
-        false);
+    const profitManager = new ProfitManager(to, from, investors, requestManager, false);
     profitManager.updateProfit(value);
   }
 
-  async tx_crushOnPost(from, to, pid, value) {
+  async tx_crushOnPost (from, to, pid, value) {
     const requestManager = new RequestManager(this.endpoint, APP_NAME);
     await this.shareProfit(requestManager, from, to, value);
     requestManager.increasePostCrushOn(to, pid, from, value);
@@ -51,7 +50,7 @@ class AfanClient {
     return requestManager.send();
   }
 
-  async tx_crushOnReply(from, to, pid, rid, value) {
+  async tx_crushOnReply (from, to, pid, rid, value) {
     const requestManager = new RequestManager(this.endpoint, APP_NAME);
     await this.shareProfit(requestManager, from, to, value);
     requestManager.increaseReplyCrushOn(pid, rid, from, value);
@@ -59,17 +58,17 @@ class AfanClient {
     return requestManager.send();
   }
 
-  async tx_adpropose(from, to, value, intermed) {
+  async tx_adpropose (from, to, value, intermed) {
     const requestManager = new RequestManager(this.endpoint, APP_NAME);
     try {
       const state = await requestManager.getAdState(from, to);
-      if (state.result && state.result !==3) {
-        return {code: -4, message: 'Already proposed'};
+      if (state.result && state.result !== 3) {
+        return { code: -4, message: 'Already proposed' };
       }
     } catch (err) {
-      if (!(err instanceof errors.StatusCodeError)){
+      if (!(err instanceof errors.StatusCodeError)) {
         throw err
-      } 
+      }
     }
     requestManager.increaseBalance(from, -value);
     requestManager.increaseBalance(intermed, value);
@@ -77,6 +76,5 @@ class AfanClient {
     return requestManager.send();
   }
 }
-
 
 module.exports = AfanClient;
