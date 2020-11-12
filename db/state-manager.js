4df0128014fc45ed2370749f52ca0615e22bb648
Dongil Seo <seo@comcom.ai>
2020-11-12 20:10:28 +0900

    Add more functions to StateManager.

--- db/state-manager.js
+++ db/state-manager.js
@@ -1,35 +1,95 @@
 const StateNode = require('./state-node');
+const { deleteStateTree } = require('./state-util');
 
-const BASE_VERSION = 'base';
+const INITIAL_VERSION = "init";
 
 class StateManager {
   constructor() {
     this.rootMap = new Map();
-    this.setRoot(BASE_VERSION, new StateNode());
+    this.finalizedVersion = INITIAL_VERSION;
+    this.rootMap.set(INITIAL_VERSION, new StateNode());
   }
 
-  getBaseRoot() {
-    return this.getRoot(BASE_VERSION);
+  /**
+   * Returns the finalized version.
+   */
+  getFinalizedVersion() {
+    return this.finalizedVersion;
   }
 
+  /**
+   * Returns the finalized version.
+   */
+  getFinalizedRoot() {
+    return this.getRoot(this.finalizedVersion);
+  }
+
+  /**
+   * Returns the corresponding state root of the given version if available.
+   * Otherwise, returns null.
+   * 
+   * @param {string} version 
+   */
   getRoot(version) {
     const root = this.rootMap.get(version);
-    if (root === undefined) {
-      return null;
+    return root === undefined ? null : root;
+  }
+
+  /**
+   * Returns true if the given version exists, otherwise false.
+   * 
+   * @param {string} version 
+   */
+  hasVersion(version) {
+    return this.rootMap.has(version);
+  }
+
+  /**
+   * Returns all versions.
+   */
+  getVersionList() {
+    return Array.from(this.rootMap.keys());
+  }
+
+  /**
+   * Clones the state root of the given version to create a new root with the given new version.
+   * 
+   * @param {string} version 
+   * @param {string} newVersion 
+   */
+  cloneVersion(version, newVersion) {
+    if (!this.hasVersion(version)) {
+      return false;
     }
-    return root;
+    const root = this.getRoot(version);
+    const newRoot = makeCopyOfStateTree(root);
+    this.rootMap.set(newVersion, newRoot);
+    return true;
   }
 
-  setRoot(version, root) {
-    this.rootMap.set(version, root);
-  }
-
-  cloneRoot(version, newVersion) {
-    // TODO(lia): Implement this.
-  }
-
+  /**
+   * Deletes the state roots of the given version.
+   * 
+   * @param {string} version 
+   */
   deleteVersion(version) {
-    // TODO(lia): Implement this.
+    if (!this.hasVersion(version)) {
+      return false;
+    }
+    this.rootMap.delete(version);
+    const root = this.getRoot(version);
+    deleteStateTree(root);
+    return true;
+  }
+
+  /**
+   * Finalize the given version by deleting all state roots of versions lower than
+   * the given version.
+   * 
+   * @param {string} version 
+   */
+  finalizeVersion(version) {
+    this.finalizeVersion = version;
   }
 }
 
