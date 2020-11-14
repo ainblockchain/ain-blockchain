ec3333d48648653b716f949712c172b654035d6c
Dongil Seo <seo@comcom.ai>
2020-11-15 00:42:56 +0900

    Move initDbData() call out of DB constructor.

--- blockchain/block.js
+++ blockchain/block.js
@@ -245,6 +245,7 @@ class Block {
 
   static getGenesisStateProofHash() {
     const tempGenesisState = new DB(new StateNode(), null, null, false, -1);
+    tempGenesisState.initDbData();
     const genesisTransactions = Block.getGenesisBlockData(
         GenesisAccounts[AccountProperties.TIMESTAMP]);
     for (const tx of genesisTransactions) {
