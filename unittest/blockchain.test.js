ac4d15a73ffafeabc2887317d314a7b27562f463
Dongil Seo <seo@comcom.ai>
2021-01-08 16:00:14 +0900

    Fix broken test.

--- unittest/blockchain.test.js
+++ unittest/blockchain.test.js
@@ -96,8 +96,7 @@ describe('Blockchain', () => {
       while (!node1.bc.lastBlock() || !node2.bc.lastBlock() || node1.bc.lastBlock().hash !== node2.bc.lastBlock().hash) {
         const blockSection = node1.bc.requestBlockchainSection(node2.bc.lastBlock());
         if (blockSection) {
-          node2.bc.merge(blockSection, node2.createDb(node2.stateManager.getFinalVersion(),
-              `NODE:${blockSection[blockSection.length - 1].number}`, this.bc, this.tp, true));
+          node2.mergeChainSegment(blockSection);
         }
       }
       assert.deepEqual(JSON.stringify(node1.bc.chain), JSON.stringify(node2.bc.chain));
