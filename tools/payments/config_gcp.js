6e96058dd0a86f31b56fa6c046b7f4bf6b11a9a1
Lia Yoo <lia@comcom.ai>
2021-02-09 18:56:53 +0900

    Update endpointUrl in config_gpc.js.

--- tools/payments/config_gcp.js
+++ tools/payments/config_gcp.js
@@ -1,5 +1,5 @@
 module.exports = {
-  endpointUrl: "http://35.197.153.179:8080",
+  endpointUrl: "http://dev-node.ainetwork.ai:8080",
   userAddr: "0x09A0d53FDf1c36A131938eb379b98910e55EEfe1",
   serviceOwnerAddr: "0x08Aed7AF9354435c38d52143EE50ac839D20696b",
   serviceOwnerPrivateKey: "9fad756c0bd0d3a42643973f36e61d4d76e01cbb41371fa3046bcced6926e1b2"
