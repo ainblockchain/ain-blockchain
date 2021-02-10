0d27f15713ab009e251478ab77cfa07dae8ff74e
Dongil Seo <seo@comcom.ai>
2021-02-11 04:04:40 +0900

    Increase max iteration to 200. Fix broken tests.

--- unittest/test-util.js
+++ unittest/test-util.js
@@ -70,7 +70,7 @@ function addBlock(node, txs, votes, validators) {
 }
 
 function waitUntilTxFinalized(servers, txHash) {
-  const MAX_ITERATION = 100;
+  const MAX_ITERATION = 200;
   let iterCount = 0;
   const unchecked = new Set(servers);
   while (true) {
