0518a327f26c33383c3447817ee9ecc8493c959d
Dongil Seo <seo@comcom.ai>
2021-02-16 19:39:07 +0900

    Handle deleted nodes properly in updateProofhashForAllRootPaths().

--- db/state-util.js
+++ db/state-util.js
@@ -297,10 +297,10 @@ function updateProofHashForAllRootPaths(fullPath, root) {
     const label = fullPath[i];
     const child = node.getChild(label);
     if (child === null) {
-      logger.error(
+      logger.info(
           `[${LOG_HEADER}] Trying to update proof hash for non-existing path: ` +
           `${ChainUtil.formatPath(fullPath.slice(0, i + 1))}.`);
-      return 0;
+      break;
     }
     node = child;
   }
