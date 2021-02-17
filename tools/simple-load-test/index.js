f1d3692d1216cc8c61688a311ffa498582c18b8f
pengin7384 <pengin7384@gmail.com>
2021-02-17 14:24:43 +0900

    Init load-test

---
+++ tools/simple-load-test/index.js
@@ -0,0 +1,129 @@
+/*
+For simple load testing
+This tool increases '/apps/loadtest/visit_count'
+Please modify numberOfTransactions & duration
+ */
+const Ain = require('@ainblockchain/ain-js').default;
+const delay = (time) => new Promise((resolve) => setTimeout(resolve, time));
+const testPath = '/apps/loadtest';
+const ainUrl = 'http://localhost:8081';
+const ainPrivateKey = '4207f5dcacb1b601d3a1f8cb10afaca158f6ebe383c0b30d02b39f8d2060cce3';
+const ainAddress = '0xF2be7f1356347a8960630c112AcB6Da61eE94632';
+const numberOfTransactions = 300;
+const duration = 10; // 60: 1min
+const ain = new Ain(ainUrl);
+
+function initAinJs() {
+  ain.wallet.add(ainPrivateKey);
+  ain.wallet.setDefaultAccount(ainAddress);
+  ain.provider.setDefaultTimeoutMs(60 * 1000);
+}
+
+async function initPermission() {
+  const setOwnerTx = {
+    operation: {
+      type: 'SET_OWNER',
+      ref: testPath,
+      value: {
+        '.owner': {
+          owners: {
+            '*': {
+              write_owner: true,
+              write_rule: true,
+              write_function: true,
+              branch_owner: true,
+            },
+          },
+        },
+      },
+      is_global: true,
+    },
+    nonce: -1,
+  };
+  const setRuleTx = {
+    operation: {
+      type: 'SET_RULE',
+      ref: testPath,
+      value: {
+        '.write': true,
+      },
+      is_global: true,
+    },
+    nonce: -1,
+  };
+  const setValueTx = {
+    operation: {
+      type: 'SET_VALUE',
+      ref: testPath,
+      value: 0,
+      is_global: true,
+    },
+    nonce: -1,
+  };
+  await ain.sendTransactionBatch([
+    setOwnerTx,
+    setRuleTx,
+    setValueTx,
+  ]);
+
+  await delay(10 * 1000);
+}
+
+function makeBaseTransaction() {
+  return {
+    operation: {
+      type: 'INC_VALUE',
+      ref: `${testPath}/visit_count`,
+      value: 1,
+    },
+    nonce: -1,
+  };
+}
+
+async function sendTxs() {
+  const delayTime = duration / numberOfTransactions * 1000;
+  const sendTxPromiseList = [];
+  const timestamp = Date.now();
+  const baseTx = makeBaseTransaction();
+  let sendCnt = 0;
+
+  for (let i = 0; i < numberOfTransactions; i++) {
+    await delay(delayTime);
+    if (i % 1000 === 0) {
+      console.log(`[${i}/${numberOfTransactions}]`);
+    }
+
+    sendTxPromiseList.push(
+        new Promise((resolve, reject) => {
+          setTimeout((txTimestamp) => {
+            baseTx.timestamp = txTimestamp;
+            ain.sendTransaction(baseTx).then((result) => {
+              if (!result || !result.hasOwnProperty('tx_hash')) {
+                throw Error(`Wrong format`);
+              } else if (!result.result) {
+                throw Error('result !== true');
+              }
+              sendCnt++;
+              resolve(true);
+            }).catch((err) => {
+              resolve(false);
+            });
+          }, 0, timestamp + i);
+        }),
+    );
+  }
+
+  await Promise.all(sendTxPromiseList);
+  return sendCnt;
+}
+
+async function main() {
+  initAinJs();
+  console.log(`Initialize permission (${testPath})`);
+  await initPermission();
+  console.log(`Start to send transactions (${numberOfTransactions})`)
+  const sendCnt = await sendTxs();
+  console.log(`Finish load test! (${sendCnt}/${numberOfTransactions})`);
+}
+
+main();
