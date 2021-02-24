8cc1c569841f9103a3306eb14c4094383685c010
Dongil Seo <seo@comcom.ai>
2021-02-24 22:58:24 +0900

    Give -y option to apt-get install.

--- setup_monitoring_ubuntu.sh
+++ setup_monitoring_ubuntu.sh
@@ -45,4 +45,4 @@ wget -q -O - https://packages.grafana.com/gpg.key | sudo apt-key add -
 echo "deb https://packages.grafana.com/oss/deb stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
 
 sudo apt-get update
-sudo apt-get install grafana
+sudo apt-get install -y grafana
