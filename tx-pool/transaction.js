2d3559f158630ca808a1d3ca0b80623497170225
Dongil Seo <seo@comcom.ai>
2021-03-05 17:03:04 +0900

    Tweak transaction methods.

--- tx-pool/transaction.js
+++ tx-pool/transaction.js
@@ -1,6 +1,5 @@
 const _ = require('lodash');
 const ainUtil = require('@ainblockchain/ain-util');
-const stringify = require('fast-json-stable-stringify');
 const logger = require('../logger')('TRANSACTION');
 const { WriteDbOperations } = require('../common/constants');
 const ChainUtil = require('../common/chain-util');
@@ -29,8 +28,7 @@ class Transaction {
       return null;
     }
 
-    const sanitized = Transaction.sanitizeTxBody(txBody);
-    const hash = '0x' + ainUtil.hashTransaction(sanitized).toString('hex');
+    const hash = '0x' + ainUtil.hashTransaction(txBody).toString('hex');
 
     let address = null;
     let skipVerif = null;
@@ -164,17 +162,13 @@ class Transaction {
   }
 
   static verifyTransaction(tx) {
-    if (tx.tx_body !== undefined &&
-        tx.tx_body.operation !== undefined &&
-        tx.tx_body.operation.type !== undefined &&
-        Object.keys(WriteDbOperations).indexOf(tx.tx_body.operation.type) === -1) {
-      logger.info(`Invalid transaction type: ${tx.tx_body.operation.type}`);
+    if (!tx || !Transaction.isValidTxBody(tx.tx_body)) {
+      logger.info(`Invalid transaction body: ${JSON.stringify(tx, null, 2)}`);
       return false;
     }
     // A devel method for bypassing the transaction verification.
-    if (tx.extra.skip_verif) {
-      logger.info('Skip verifying signature for transaction: ' +
-          JSON.stringify(tx, null, 2));
+    if (_.get(tx, 'extra.skip_verif')) {
+      logger.info('Skip verifying signature for transaction: ' + JSON.stringify(tx, null, 2));
       return true;
     }
     return ainUtil.ecVerifySig(tx.tx_body, tx.signature, tx.address);
@@ -182,13 +176,21 @@ class Transaction {
 
   static isValidTxBody(txBody) {
     if (!Transaction.hasRequiredFields(txBody)) {
-      logger.info(
-          `Transaction body with missing timestamp, operation or nonce: ` +
-          `${JSON.stringify(txBody, null, 2)}`);
+      logger.info(`Transaction body has some missing fields: ${JSON.stringify(txBody, null, 2)}`);
       return false;
     }
+    return Transaction.isInStandardFormat(txBody);
+  }
+
+  static hasRequiredFields(txBody) {
+    return txBody && txBody.timestamp !== undefined && txBody.nonce !== undefined &&
+        txBody.operation !== undefined;
+  }
+
+  static isInStandardFormat(txBody) {
     const sanitized = Transaction.sanitizeTxBody(txBody);
-    if (!Transaction.isValidFormat(txBody)) {
+    const isIdentical = _.isEqual(JSON.parse(JSON.stringify(sanitized)), txBody, { strict: true });
+    if (!isIdentical) {
       logger.info(
           `Transaction body in a non-standard format ` +
           `- input:\n${JSON.stringify(txBody, null, 2)}\n\n` +
@@ -198,22 +200,11 @@ class Transaction {
     return true;
   }
 
-  static hasRequiredFields(txBody) {
-    return txBody.timestamp !== undefined && txBody.nonce !== undefined &&
-        txBody.operation !== undefined;
-  }
-
-  static isValidFormat(txBody) {
-    const sanitized = Transaction.sanitizeTxBody(txBody);
-    return _.isEqual(JSON.parse(JSON.stringify(sanitized)), txBody, { strict: true });
-  }
-
   static isBatchTxBody(txBody) {
-    return Array.isArray(txBody.tx_list);
+    return tx && Array.isArray(tx.tx_list);
   }
-
   static isBatchTransaction(tx) {
-    return Array.isArray(tx.tx_list);
+    return Transaction.isBatchTxBody(tx);
   }
 }
 
