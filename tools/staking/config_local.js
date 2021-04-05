eca4f318d77b2a11177a9c8a6ed83a6c0b05ccf7
Lia Yoo <lia@comcom.ai>
2021-04-05 11:49:48 +0900

    Add staking tool scripts.

---
+++ tools/staking/config_local.js
@@ -0,0 +1,7 @@
+module.exports = {
+  endpointUrl: "http://localhost:8081",
+  userAddr: "0x09A0d53FDf1c36A131938eb379b98910e55EEfe1",
+  userPrivateKey: "ee0b1315d446e5318eb6eb4e9d071cd12ef42d2956d546f9acbdc3b75c469640",
+  serviceOwnerAddr: "0x08Aed7AF9354435c38d52143EE50ac839D20696b",
+  serviceOwnerPrivateKey: "9fad756c0bd0d3a42643973f36e61d4d76e01cbb41371fa3046bcced6926e1b2"
+};
---
+++ tools/staking/sendUnstakeTx.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const { signAndSendTx, confirmTransaction } = require('../util');
+
+function buildUnstakeTxBody(address, timestamp) {
+  return {
+    operation: {
+      type: 'SET_VALUE',
+      ref: `/staking/test_service/${address}/0/unstake/${timestamp}/value`,
+      value: 100
+    },
+    timestamp,
+    nonce: -1
+  }
+}
+
+async function sendTransaction() {
+  console.log('\n*** sendTransaction():');
+  const timestamp = Date.now();
+  const txBody = buildUnstakeTxBody(config.userAddr, timestamp);
+  console.log(`txBody: ${JSON.stringify(txBody, null, 2)}`);
+  const txInfo = await signAndSendTx(config.endpointUrl, txBody, config.userPrivateKey);
+  console.log(`txInfo: ${JSON.stringify(txInfo, null, 2)}`);
+  if (txInfo.success) {
+    await confirmTransaction(config.endpointUrl, timestamp, txInfo.txHash);
+  }
+}
+
+async function processArguments() {
+  if (process.argv.length !== 3) {
+    usage();
+  }
+  config = require(path.resolve(__dirname, process.argv[2]));
+  await sendTransaction();
+}
+
+function usage() {
+  console.log('\nExample commandlines:\n  node sendUnstakeTx.js config_local.js\n')
+  process.exit(0)
+}
+
+processArguments();
