2f3fe3e1cdca04ddcb66ddcd2185a0bac3c02d65
minsulee2 <minsu@comcom.ai>
2021-05-27 19:14:40 +0900

    Shut down wsServer via process kill

--- integration/p2p.test.js
+++ integration/p2p.test.js
@@ -1,5 +1,6 @@
 const chai = require('chai');
 const url = require('url');
+const process = require('process');
 
 const BlockchainNode = require('../node');
 const VersionUtil = require('../common/version-util');
@@ -34,6 +35,13 @@ describe("p2p", () => {
 
   after(() => {
     p2pClient.stop();
+    // This is added since p2pClient.stop() calls wsServer.close() though, it does not really close
+    // the server but still some wsServer in process leaves somehow(perhaps intended? or bug?).
+    // So that, the server should be manully shut down via process.kill().
+    // See also: https://github.com/websockets/ws/blob/master/doc/ws.md#class-websocketserver
+    if (process.ppid) {
+      process.kill(process.ppid);
+    }
   });
 
   describe("server status", () => {
