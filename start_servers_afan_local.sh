8f392dbd3ddb8ac4e8124d2819829e38460a0bd5
Lia Yoo <lia@comcom.ai>
2021-07-20 16:53:16 +0900

    Update ports for afan child chain nodes.

--- start_servers_afan_local.sh
+++ start_servers_afan_local.sh
@@ -9,14 +9,14 @@ MIN_NUM_VALIDATORS=3 ACCOUNT_INDEX=2 CONSOLE_LOG=true ENABLE_DEV_SET_CLIENT_API=
 sleep 15
 
 # AFAN CHILD CHAIN
-GENESIS_CONFIGS_DIR=genesis-configs/afan-shard PORT=9010 P2P_PORT=6000 CONSOLE_LOG=true BLOCKCHAIN_DATA_DIR=~/ain_blockchain_data node ./tracker-server/index.js &
+GENESIS_CONFIGS_DIR=genesis-configs/afan-shard PORT=9000 P2P_PORT=6000 CONSOLE_LOG=true BLOCKCHAIN_DATA_DIR=~/ain_blockchain_data node ./tracker-server/index.js &
 sleep 5
-GENESIS_CONFIGS_DIR=genesis-configs/afan-shard PORT=9011 P2P_PORT=6001 MIN_NUM_VALIDATORS=3 ACCOUNT_INDEX=0 CONSOLE_LOG=true ENABLE_DEV_SET_CLIENT_API=true ENABLE_TX_SIG_VERIF_WORKAROUND=true ENABLE_GAS_FEE_WORKAROUND=true BLOCKCHAIN_DATA_DIR=~/ain_blockchain_data node ./client/index.js &
+GENESIS_CONFIGS_DIR=genesis-configs/afan-shard PORT=9001 P2P_PORT=6001 MIN_NUM_VALIDATORS=3 ACCOUNT_INDEX=0 CONSOLE_LOG=true ENABLE_DEV_SET_CLIENT_API=true ENABLE_TX_SIG_VERIF_WORKAROUND=true ENABLE_GAS_FEE_WORKAROUND=true BLOCKCHAIN_DATA_DIR=~/ain_blockchain_data node ./client/index.js &
 sleep 5
 
 while :
 do
-    nodeState=$(curl -m 20 -X GET -H "Content-Type: application/json" "http://localhost:9011/node_status" | jq -r '.result.state')
+    nodeState=$(curl -m 20 -X GET -H "Content-Type: application/json" "http://localhost:9001/node_status" | jq -r '.result.state')
     printf "\nnodeState = ${nodeState}\n"
     if [[ "$nodeState" = "SERVING" ]]; then
         printf "\nShard node 0 is now serving!\n"
@@ -25,7 +25,7 @@ do
     sleep 20
 done
 
-GENESIS_CONFIGS_DIR=genesis-configs/afan-shard PORT=9012 P2P_PORT=6002 MIN_NUM_VALIDATORS=3 ACCOUNT_INDEX=1 CONSOLE_LOG=true ENABLE_DEV_SET_CLIENT_API=true ENABLE_TX_SIG_VERIF_WORKAROUND=true ENABLE_GAS_FEE_WORKAROUND=true BLOCKCHAIN_DATA_DIR=~/ain_blockchain_data node ./client/index.js &
+GENESIS_CONFIGS_DIR=genesis-configs/afan-shard PORT=9002 P2P_PORT=6002 MIN_NUM_VALIDATORS=3 ACCOUNT_INDEX=1 CONSOLE_LOG=true ENABLE_DEV_SET_CLIENT_API=true ENABLE_TX_SIG_VERIF_WORKAROUND=true ENABLE_GAS_FEE_WORKAROUND=true BLOCKCHAIN_DATA_DIR=~/ain_blockchain_data node ./client/index.js &
 sleep 5
-GENESIS_CONFIGS_DIR=genesis-configs/afan-shard PORT=9013 P2P_PORT=6003 MIN_NUM_VALIDATORS=3 ACCOUNT_INDEX=2 CONSOLE_LOG=true ENABLE_DEV_SET_CLIENT_API=true ENABLE_TX_SIG_VERIF_WORKAROUND=true ENABLE_GAS_FEE_WORKAROUND=true BLOCKCHAIN_DATA_DIR=~/ain_blockchain_data node ./client/index.js &
+GENESIS_CONFIGS_DIR=genesis-configs/afan-shard PORT=9003 P2P_PORT=6003 MIN_NUM_VALIDATORS=3 ACCOUNT_INDEX=2 CONSOLE_LOG=true ENABLE_DEV_SET_CLIENT_API=true ENABLE_TX_SIG_VERIF_WORKAROUND=true ENABLE_GAS_FEE_WORKAROUND=true BLOCKCHAIN_DATA_DIR=~/ain_blockchain_data node ./client/index.js &
 sleep 15
