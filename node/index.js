6d8c0f3a3f0ecc87ae4099f666f17ae628553742
Dongil Seo <seo@comcom.ai>
2021-07-21 21:18:24 +0900

    Divide IN_POOL tx state into EXECUTED and PENDING states.

--- node/index.js
+++ node/index.js
@@ -20,6 +20,7 @@ const {
   ShardingProtocols,
   GenesisAccounts,
   GenesisSharding,
+  TransactionStates,
   StateVersions,
   SyncModeOptions,
   LIGHTWEIGHT,
@@ -238,6 +239,32 @@ class BlockchainNode {
 
   dumpFinalVersion(withDetails) {
     return this.stateManager.getFinalRoot().toJsObject(withDetails);
+  }
+
+  getTransactionByHash(hash) {
+    const transactionInfo = this.tp.transactionTracker[hash];
+    if (!transactionInfo) {
+      return null;
+    }
+
+    if (transactionInfo.state === TransactionStates.IN_BLOCK) {
+      const block = this.bc.getBlockByNumber(transactionInfo.number);
+      const index = transactionInfo.index;
+      if (!block) {
+        // TODO(liayoo): Ask peers for the transaction / block
+      } else if (index >= 0) {
+        transactionInfo.transaction = block.transactions[index];
+      } else {
+        transactionInfo.transaction =
+            _.find(block.last_votes, (tx) => tx.hash === args.hash) || null;
+      }
+    } else if (transactionInfo.state === TransactionStates.EXECUTED ||
+        transactionInfo.state === TransactionStates.PENDING) {
+      const address = transactionInfo.address;
+      transactionInfo.transaction =
+          _.find(this.tp.transactions[address], (tx) => tx.hash === args.hash) || null;
+    }
+    return transactionInfo;
   }
 
   getNonceForAddr(address, fromPending, fromDb = false) {
