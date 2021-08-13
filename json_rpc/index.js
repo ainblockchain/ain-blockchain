ad742ef1fd5fc593718365ad605b26d0fb5c7b27
minsulee2 <minsu@comcom.ai>
2021-08-13 10:40:40 +0900

    Update NETWORK_ID

--- json_rpc/index.js
+++ json_rpc/index.js
@@ -374,35 +374,35 @@ module.exports = function getMethods(node, p2pServer, minProtocolVersion, maxPro
     },
 
     // Network API
-    net_listening: function(args, done) {
+    net_listening: function (args, done) {
       const peerCount = Object.keys(p2pServer.inbound).length;
-      done(null, addProtocolVersion({result: !!peerCount}));
+      done(null, addProtocolVersion({ result: !!peerCount }));
     },
 
-    net_peerCount: function(args, done) {
+    net_peerCount: function (args, done) {
       const peerCount = Object.keys(p2pServer.inbound).length;
-      done(null, addProtocolVersion({result: peerCount}));
+      done(null, addProtocolVersion({ result: peerCount }));
     },
 
-    net_syncing: function(args, done) {
+    net_syncing: function (args, done) {
       // TODO(liayoo): Return { starting, latest } with block numbers
       // if the node is currently syncing.
       done(null, addProtocolVersion(
-          {result: p2pServer.node.state === BlockchainNodeStates.SYNCING}));
+        { result: p2pServer.node.state === BlockchainNodeStates.SYNCING }));
     },
 
-    net_getNetworkId: function(args, done) {
-      done(null, addProtocolVersion({result: NETWORK_ID}));
+    net_getNetworkId: function (args, done) {
+      done(null, addProtocolVersion({ result: NETWORK_ID }));
     },
 
-    net_consensusStatus: function(args, done) {
+    net_consensusStatus: function (args, done) {
       const result = p2pServer.consensus.getStatus();
-      done(null, addProtocolVersion({result}));
+      done(null, addProtocolVersion({ result }));
     },
 
-    net_rawConsensusStatus: function(args, done) {
+    net_rawConsensusStatus: function (args, done) {
       const result = p2pServer.consensus.getRawStatus();
-      done(null, addProtocolVersion({result}));
+      done(null, addProtocolVersion({ result }));
     }
   };
 };
