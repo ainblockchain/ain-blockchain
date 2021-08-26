eed1dac8e6bece596ca37deb3227991b9fbda3cc
pengin7384 <pengin7384@gmail.com>
2021-08-27 02:30:10 +0900

    Add setHealthCareValue.js

---
+++ tools/he-health-care/setHealthCareValue.js
@@ -0,0 +1,25 @@
+const { signAndSendTx } = require('../util');
+const { healthCareAppName, ainUrl, ainPrivateKey } = require('./constants');
+
+function buildSetValueTxBody(appName, timestamp) {
+  const dummy = 'a'.repeat(4 * 1024); // 8KB
+  return {
+    operation: {
+      type: 'SET_VALUE',
+      ref: `/apps/${appName}/tasks/${timestamp}`,
+      value: dummy,
+    },
+    timestamp,
+    nonce: -1,
+  };
+}
+
+async function main() {
+  const setValueTxBody = buildSetValueTxBody(healthCareAppName, Date.now());
+  const setValueResult = await signAndSendTx(ainUrl, setValueTxBody, ainPrivateKey);
+  if (!setValueResult.success) {
+    throw Error(`Can't set value (${JSON.stringify(setValueResult, null, 2)})`);
+  }
+}
+
+main();
