0b79c70d802a3edad9010e2d972ad86d918f4bda
pengin7384 <pengin7384@gmail.com>
2021-08-27 02:29:07 +0900

    Add createHealthCareApp.js

---
+++ tools/he-health-care/createHealthCareApp.js
@@ -0,0 +1,28 @@
+const { signAndSendTx } = require('../util');
+const { healthCareAppName, ainUrl, ainPrivateKey, ainAddress } = require('./constants');
+
+function buildCreateAppTxBody(appName, timestamp) {
+  return {
+    operation: {
+      type: 'SET_VALUE',
+      ref: `/manage_app/${appName}/create/${timestamp}`,
+      value: {
+        admin: {
+          [ainAddress]: true,
+        },
+      },
+    },
+    timestamp,
+    nonce: -1,
+  };
+}
+
+async function main() {
+  const createHealthCareAppTxBody = buildCreateAppTxBody(healthCareAppName, Date.now());
+  const createResult = await signAndSendTx(ainUrl, createHealthCareAppTxBody, ainPrivateKey);
+  if (!createResult.success) {
+    throw Error(`Can't create health care app (${JSON.stringify(createResult, null, 2)})`);
+  }
+}
+
+main();
