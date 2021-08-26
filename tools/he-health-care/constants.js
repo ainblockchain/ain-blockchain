626a794e07ef17a7d2ddcf835b16e470819f3996
pengin7384 <pengin7384@gmail.com>
2021-08-27 02:28:56 +0900

    Add he-health-care constants

---
+++ tools/he-health-care/constants.js
@@ -0,0 +1,11 @@
+const ainAddress = '0xb2585543Cfcfb79CF73a1a14b2DfBC411913940F';
+const ainPrivateKey = 'd910c1835eaa89f15452aa3f0bd95f61fb9a04464150e37d617a40ed0071558c';
+const ainUrl = 'http://localhost:8081';
+const healthCareAppName = 'he_health_care';
+
+module.exports = {
+  ainAddress,
+  ainPrivateKey,
+  ainUrl,
+  healthCareAppName,
+};
