c267cd88b2f74c4326825f0b27101458171417cc
pengin7384 <pengin7384@gmail.com>
2021-08-27 11:06:17 +0900

    Change ain~~~ to serviceOwner

--- tools/he-health-care/sendSetRuleTx.js
+++ tools/he-health-care/sendSetRuleTx.js
@@ -1,5 +1,5 @@
 const { signAndSendTx } = require('../util');
-const { healthCareAppName, ainUrl, ainPrivateKey } = require('./config_local');
+const { healthCareAppName, ainUrl, serviceOwnerPrivateKey } = require('./config_local');
 
 function buildSetRuleTxBody(appName, timestamp) {
   return {
@@ -19,7 +19,7 @@ function buildSetRuleTxBody(appName, timestamp) {
 
 async function main() {
   const setRuleTxBody = buildSetRuleTxBody(healthCareAppName, Date.now());
-  const setRuleResult = await signAndSendTx(ainUrl, setRuleTxBody, ainPrivateKey);
+  const setRuleResult = await signAndSendTx(ainUrl, setRuleTxBody, serviceOwnerPrivateKey);
   if (!setRuleResult.success) {
     throw Error(`Can't set rule (${JSON.stringify(setRuleResult, null, 2)})`);
   }
--- tools/he-health-care/sendTaskRequestTx.js
+++ tools/he-health-care/sendTaskRequestTx.js
@@ -1,5 +1,5 @@
 const { signAndSendTx } = require('../util');
-const { healthCareAppName, ainUrl, ainPrivateKey } = require('./config_local');
+const { healthCareAppName, ainUrl, serviceOwnerPrivateKey } = require('./config_local');
 
 function buildSetValueTxBody(appName, timestamp) {
   const dummy = 'a'.repeat(4 * 1024); // 8KB
@@ -16,7 +16,7 @@ function buildSetValueTxBody(appName, timestamp) {
 
 async function main() {
   const setValueTxBody = buildSetValueTxBody(healthCareAppName, Date.now());
-  const setValueResult = await signAndSendTx(ainUrl, setValueTxBody, ainPrivateKey);
+  const setValueResult = await signAndSendTx(ainUrl, setValueTxBody, serviceOwnerPrivateKey);
   if (!setValueResult.success) {
     throw Error(`Can't set value (${JSON.stringify(setValueResult, null, 2)})`);
   }
