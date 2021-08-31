f214bdbf58c8e8325a4c54c736d3497dd615514a
Dongil Seo <seo@comcom.ai>
2021-08-31 18:36:07 +0900

    Rename: hexLabelMap -> hexLabelCache.

--- common/constants.js
+++ common/constants.js
@@ -31,8 +31,8 @@ const FeatureFlags = {
   enableReceiptPathPrefixLayers: false,  // Some test cases assume this value false.
   // Enables radix layers.
   enableRadixTreeLayers: true,  // Some test cases assume this value true.
-  // Enables hex label map.
-  enableHexLabelMap: true,
+  // Enables hex label cache.
+  enableHexLabelCache: true,
   // Enables array radix child map.
   enableArrayRadixChildMap: true,
 };
