5adb04fb37d824df7370401cf10cd2146c5a8626
minsulee2 <minsu@comcom.ai>
2021-09-01 16:25:38 +0900

    Remove bound limits

--- common/constants.js
+++ common/constants.js
@@ -650,11 +650,8 @@ function initializeNetworkEnvronments() {
     return GenesisParams.network;
   } else {
     return {
-      // NOTE(minsulee2): Need a discussion that MAX_NUM_VALIDATORS and MAX_INBOUND_LIMIT
-      // should not be related to one another.
       P2P_MESSAGE_TIMEOUT_MS: 600000,
-      MAX_OUTBOUND_LIMIT: GenesisParams.consensus.MAX_NUM_VALIDATORS - 1,
-      MAX_INBOUND_LIMIT: GenesisParams.consensus.MAX_NUM_VALIDATORS - 1,
+      LIMIT_NUMBER_OF_CANDIDATES_AT_ONCE: 2,
       DEFAULT_MAX_OUTBOUND: GenesisParams.consensus.MAX_NUM_VALIDATORS - 1,
       DEFAULT_MAX_INBOUND: GenesisParams.consensus.MAX_NUM_VALIDATORS - 1
     }
