8c04e916c8182418a71a090b37b06644ce69fe27
minsulee2 <minsu@comcom.ai>
2021-09-10 17:16:47 +0900

    Get graph data from tracker

--- tools/network/index.js
+++ tools/network/index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const axios = require('axios').default;
+const commonUtil = require('../../common/common-util');
 
 const app = express();
 
@@ -7,22 +9,49 @@ const PORT = 8000;
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 
-app.get('/', (req, res) => {
-  res.render(__dirname + '/index.html', {}, (err, html) => {
-    const data = {
+const buildGraphData = (peerNodes) => {
+  const data = { nodes: [], links: [] };
+  const peerNodeIdMap = { };
+
+  Object.keys(peerNodes).forEach((peerNode, i) => {
+    Object.assign(peerNodeIdMap, { [peerNode]: i });
+    data.nodes.push({ address: peerNode });
+  });
+
+  Object.values(peerNodes).forEach(nodeInfo => {
+    const id = peerNodeIdMap[nodeInfo.address];
+    const outGoingList = nodeInfo.networkStatus.connectionStatus.outgoingPeers;
+    outGoingList.forEach(address => {
+      data.links.push({ source: id, target: peerNodeIdMap[address], weight: 1 });
+    });
+  });
+
+  return data;
+}
+
+const getGraphData = async () => {
+  const networkStatusResponse = await axios.get('http://localhost:8080/network_status');
+  const networkStatus = networkStatusResponse.data;
+  if (!commonUtil.isEmpty(networkStatus.peerNodes)) {
+    const data = buildGraphData(networkStatus.peerNodes);
+    return data;
+  } else {
+    return {
       "nodes": [
-        { "address": "node0" },
-        { "address": "node1" },
-        { "address": "node2" },
-        { "address": "node3" }
+        { "address": "Tracker is NOT online." },
+        { "address": "Tracker is NOT online." }
       ],
       "links": [
-        { "source": 2, "target": 1, "weight": 1 },
-        { "source": 0, "target": 2, "weight": 1 },
-        { "source": 2, "target": 3, "weight": 1 }
-      ]
+        { "source": 0, "target": 1, "weight": 1 }
+      ],
     };
-  html = html.replace(/{ \/\* replace this \*\/ };/g, JSON.stringify(data));
+  }
+}
+
+app.get('/', (req, res) => {
+  res.render(__dirname + '/index.html', {}, async (err, html) => {
+    const data = await getGraphData();
+    html = html.replace(/{ \/\* replace this \*\/ };/g, JSON.stringify(data));
     res.send(html);
   });
 });
