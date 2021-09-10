c89b0e8388251c8a75e9d2dff6a1ac96948f19a3
Lia Yoo <lia@comcom.ai>
2021-09-10 16:10:53 +0900

    Add check-in tool scripts.

---
+++ tools/checkin/config_local.js
@@ -0,0 +1,10 @@
+module.exports = {
+  endpointUrl: "http://localhost:8081",
+  userAddr: "0x09A0d53FDf1c36A131938eb379b98910e55EEfe1",
+  userPrivateKey: "ee0b1315d446e5318eb6eb4e9d071cd12ef42d2956d546f9acbdc3b75c469640",
+  senderAddr: "0x09A0d53FDf1c36A131938eb379b98910e55EEfe1",
+  tokenPoolAddr: "0x20ADd3d38405ebA6338CB9e57a0510DEB8f8e000",
+  tokenPoolPrivateKey: "d42f73de4ee706a4891dad643e0a65c0677020dbc2425f585442d0de2c742a44",
+  checkinId: "0",
+  tokenAmount: 100
+};
