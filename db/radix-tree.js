d5bed1afdf79e24bea014f130db4b2b589ceb643
Dongil Seo <seo@comcom.ai>
2021-09-17 17:56:35 +0900

    Deprecate terminal radix node cache.

--- db/radix-tree.js
+++ db/radix-tree.js
@@ -14,9 +14,6 @@ class RadixTree {
   constructor(version = null, parentStateNode = null) {
     this.root = new RadixNode(version, parentStateNode);
     this.numTerminalNodes = 0;
-    if (FeatureFlags.enableTerminalRadixNodeCache) {
-      this.terminalNodeCache = new Map();
-    }
   }
 
   clone(version, parentStateNode) {
@@ -56,12 +53,6 @@ class RadixTree {
   }
 
   _getRadixNodeForReading(stateLabel) {
-    if (FeatureFlags.enableTerminalRadixNodeCache) {
-      const cachedNode = this.terminalNodeCache.get(stateLabel);
-      if (cachedNode !== undefined) {
-        return cachedNode;
-      }
-    }
     const radixLabel = RadixTree._toRadixLabel(stateLabel);
     let curNode = this.root
     let labelIndex = 0;
@@ -215,12 +206,6 @@ class RadixTree {
           `${node.getLabel()} at: ${new Error().stack}.`);
       return null;
     }
-    if (FeatureFlags.enableTerminalRadixNodeCache) {
-      const cachedNode = this.terminalNodeCache.get(stateLabel);
-      if (cachedNode !== node) {
-        this.terminalNodeCache.set(stateLabel, node);
-      }
-    }
     return node.getChildStateNode();
   }
 
@@ -232,12 +217,6 @@ class RadixTree {
     const node = this._getRadixNodeForSetting(stateLabel);
     if (!node.hasChildStateNode()) {
       this.numTerminalNodes++;
-    }
-    if (FeatureFlags.enableTerminalRadixNodeCache) {
-      const cachedNode = this.terminalNodeCache.get(stateLabel);
-      if (cachedNode !== node) {
-        this.terminalNodeCache.set(stateLabel, node);
-      }
     }
     node.setChildStateNode(stateNode);
   }
@@ -311,12 +290,6 @@ class RadixTree {
       return false;
     }
     node.resetChildStateNode();
-    if (FeatureFlags.enableTerminalRadixNodeCache) {
-      const cachedNode = this.terminalNodeCache.get(stateLabel);
-      if (cachedNode !== undefined) {
-        this.terminalNodeCache.delete(stateLabel);
-      }
-    }
     this.numTerminalNodes--;
     const labelRadix = node.getLabelRadix();
     let nodesToUpdate = [node];
