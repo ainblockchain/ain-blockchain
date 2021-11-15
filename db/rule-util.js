a427a00f15fb1be82c2bb2f4b1b85f7e7d3f61e8
minsulee2 <minsu@comcom.ai>
2021-11-15 16:25:34 +0900

    Update p2p unit tests

--- db/rule-util.js
+++ db/rule-util.js
@@ -315,7 +315,7 @@ class RuleUtil {
   // NOTE(liayoo): Applies a stricter rule than isValidUrlWhitelistItem() does.
   // Asterisks are not allowed in the domain name, for instance.
   isValidUrl(url) {
-    const strictUrlRegex = /^(?:(?:https?|ftp):\/\/)(?:(?:(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,}))\.?)|localhost)(?::\d{2,5})?)(?:[/?#]\S*)?$/i
+    const strictUrlRegex = /^(?:(?:https?|ftp):\/\/)(?:(?:(?:\S+(?::\S*)?@)?(?:(?!(?:10|127|172)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,}))\.?)|localhost)(?::\d{2,5})?)(?:[/?#]\S*)?$/i
     return this.isString(url) ? strictUrlRegex.test(url) : false;
   }
 
