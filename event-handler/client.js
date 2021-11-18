004054a3d86a199009e04adf4c5e7780f31437e2
pengin7384 <pengin7384@gmail.com>
2021-11-18 18:58:05 +0900

    Change client.filters type to Object

--- event-handler/client.js
+++ event-handler/client.js
@@ -1,12 +1,18 @@
+const logger = new (require('../logger'))('EVENT_HANDLER_CLIENT');
+
 class EventHandlerClient {
   constructor(id, webSocket) {
     this.id = id;
     this.webSocket = webSocket;
-    this.filters = [];
+    this.filters = {};
   }
 
   addFilter(filter) {
-    this.filters.push(filter);
+    if (!filter.id) {
+      logger.error(`Can't find id from filter (${JSON.stringify(filter, null, 2)})`);
+      return;
+    }
+    this.filters[filter.id] = filter;
   }
 }
 
