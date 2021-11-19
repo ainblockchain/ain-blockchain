8798ded3661216797436c74c573d48d4e3cc7ce5
minsulee2 <minsu@comcom.ai>
2021-11-19 16:20:15 +0900

    Update send peer info logic a bit

--- p2p/index.js
+++ p2p/index.js
@@ -366,15 +366,17 @@ class P2pClient {
     const signature = signMessage(body, this.server.getNodePrivateKey());
     if (!signature) {
       logger.error('The signaure is not correctly generated. Discard the message!');
-      return;
+      return false;
     }
     const payload = encapsulateMessage(MessageTypes.ADDRESS_REQUEST,
         { body: body, signature: signature });
     if (!payload) {
       logger.error('The peerInfo message cannot be sent because of msg encapsulation failure.');
-      return;
+      return false;
     }
+
     socket.send(JSON.stringify(payload));
+    return true;
   }
 
   setClientSidePeerEventHandlers(socket) {
@@ -439,6 +441,7 @@ class P2pClient {
             if (!verifySignedMessage(parsedMessage, addressFromSig)) {
               logger.error(`[${LOG_HEADER}] The message is not correctly signed. ` +
                   `Discard the message!!`);
+              closeSocketSafe(this.outbound, socket);
               return;
             }
             logger.info(`[${LOG_HEADER}] A new websocket(${address}) is established.`);
@@ -603,12 +606,11 @@ class P2pClient {
     socket.on('open', async () => {
       logger.info(`Connected to peer (${url}),`);
       this.setClientSidePeerEventHandlers(socket);
-      // TODO(minsulee2): Send an encrypted form of address(pubkey can be recoverable from address),
-      // ip address, and signature.
-      this.sendPeerInfo(socket);
-      // TODO(minsulee2): Check ack from the corresponding server, then proceed reqeustChainSegment.
-      this.addPeerConnection(url);
-      this.setTimerForPeerAddressResponse(socket);
+      const isMessageSent = this.sendPeerInfo(socket);
+      if (isMessageSent) {
+        this.addPeerConnection(url);
+        this.setTimerForPeerAddressResponse(socket);
+      }
     });
   }
 
