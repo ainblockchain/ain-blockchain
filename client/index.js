448ba89dd5a28613fbca1abe034229aba6bfe081
pengin7384 <pengin7384@gmail.com>
2021-11-25 16:07:58 +0900

    Fix BlockchainNode contructor bug

--- client/index.js
+++ client/index.js
@@ -42,7 +42,7 @@ if (BlockchainConfigs.ENABLE_EXPRESS_RATE_LIMIT) {
 }
 
 const eventHandler = BlockchainConfigs.ENABLE_EVENT_HANDLER === true ? new EventHandler() : null;
-const node = new BlockchainNode(eventHandler);
+const node = new BlockchainNode(null, eventHandler);
 // NOTE(platfowner): This is very useful when the server dies without any logs.
 process.on('uncaughtException', function(err) {
   logger.error(err);
