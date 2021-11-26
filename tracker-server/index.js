ef84115ffd03a5b1b01ee726dd642dc59361486b
minsulee2 <minsu@comcom.ai>
2021-11-26 21:00:50 +0900

    Make tracker class

--- tracker-server/index.js
+++ tracker-server/index.js
@@ -1,29 +1,18 @@
 /* eslint no-unused-vars: "off" */
 const logger = new (require('../logger'))('TRACKER_SERVER');
-
-const geoip = require('geoip-lite');
 const express = require('express');
 const jayson = require('jayson');
-const _ = require('lodash');
-const disk = require('diskusage');
-const os = require('os');
-const v8 = require('v8');
 
 const { getGraphData } = require('./network-topology');
-const {
-  abbrAddr,
-  isNodeAlive
-} = require('./util');
-const { BlockchainConfigs } = require('../common/constants');
 const CommonUtil = require('../common/common-util');
+const Tracker = require('./tracker');
 
-const DISK_USAGE_PATH = os.platform() === 'win32' ? 'c:' : '/';
 const PORT = process.env.PORT || 8080;
 
-const peerNodes = {};
+const tracker = new Tracker();
 
 const app = express();
-const jsonRpcMethods = require('./json-rpc')(peerNodes);
+const jsonRpcMethods = require('./json-rpc')(tracker.peerNodes);
 app.use(express.json());
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
@@ -37,7 +26,7 @@ app.get('/', (req, res, next) => {
 });
 
 app.get('/status', (req, res, next) => {
-  const result = getStatus();
+  const result = tracker.getStatus();
   res.status(200)
       .set('Content-Type', 'application/json')
       .send(result)
@@ -46,7 +35,7 @@ app.get('/status', (req, res, next) => {
 
 // Exports metrics for Prometheus.
 app.get('/metrics', (req, res, next) => {
-  const status = getStatus();
+  const status = tracker.getStatus();
   const result = CommonUtil.objToMetrics(status);
   res.status(200)
       .set('Content-Type', 'text/plain')
@@ -55,7 +44,7 @@ app.get('/metrics', (req, res, next) => {
 });
 
 app.get('/network_status', (req, res, next) => {
-  const result = getNetworkStatus();
+  const result = tracker.getNetworkStatus();
   res.status(200)
       .set('Content-Type', 'application/json')
       .send(result)
@@ -64,7 +53,7 @@ app.get('/network_status', (req, res, next) => {
 
 app.get('/network_topology', (req, res) => {
   res.render(__dirname + '/index.html', {}, (err, html) => {
-    const networkStatus = getNetworkStatus();
+    const networkStatus = tracker.getNetworkStatus();
     const graphData = getGraphData(networkStatus);
     html = html.replace(/{ \/\* replace this \*\/ };/g, JSON.stringify(graphData));
     res.send(html);
@@ -73,7 +62,7 @@ app.get('/network_topology', (req, res) => {
 
 app.post('/update_peer_info', (req, res) => {
   const peerInfo = req.body.peerInfo;
-  setPeerNodes(peerInfo);
+  tracker.setPeerNodes(peerInfo);
   res.status(200)
       .set('Content-Type', 'application/json')
       .send({ result: 'updated' })
@@ -92,141 +81,3 @@ trackerServer.headersTimeout = 630 * 1000; // 630 seconds
 process.on('uncaughtException', function(err) {
   logger.error(err);
 });
-
-function setPeerNodes(peerInfo) {
-  peerInfo.location = getNodeLocation(peerInfo.networkStatus.ip);
-  peerNodes[peerInfo.address] = peerInfo;
-  logger.info(`Update from node [${abbrAddr(peerInfo.address)}]`);
-  logger.debug(`: ${JSON.stringify(peerInfo, null, 2)}`);
-}
-
-function getNumNodesAlive() {
-  return Object.values(peerNodes).filter(nodeInfo => isNodeAlive(nodeInfo)).length;
-}
-
-function getNodeLocation(ip) {
-  const geoLocationDict = geoip.lookup(ip);
-  if (geoLocationDict === null) {
-    return {
-      country: null,
-      region: null,
-      city: null,
-      timezone: null,
-    };
-  }
-  return {
-    country: _.isEmpty(geoLocationDict.country) ? null : geoLocationDict.country,
-    region: _.isEmpty(geoLocationDict.region) ? null : geoLocationDict.region,
-    city: _.isEmpty(geoLocationDict.city) ? null : geoLocationDict.city,
-    timezone: _.isEmpty(geoLocationDict.timezone) ? null : geoLocationDict.timezone,
-  };
-}
-
-function getNetworkStatus() {
-  return {
-    numNodesAlive: getNumNodesAlive(),
-    peerNodes
-  };
-}
-
-function getStatus() {
-  return {
-    networkStatus: {
-      numNodesAlive: getNumNodesAlive(),
-    },
-    cpuStatus: getCpuUsage(),
-    memoryStatus: getMemoryUsage(),
-    diskStatus: getDiskUsage(),
-    runtimeInfo: getRuntimeInfo(),
-    protocolInfo: getProtocolInfo(),
-  };
-}
-
-function getCpuUsage() {
-  const cores = os.cpus();
-  let free = 0;
-  let total = 0;
-  for (const core of cores) {
-    const cpuInfo = _.get(core, 'times');
-    const idle = _.get(cpuInfo, 'idle');
-    const allTimes = Object.values(cpuInfo).reduce((acc, cur) => { return acc + cur }, 0);
-    free += idle;
-    total += allTimes;
-  }
-  const usage = total - free;
-  const usagePercent = total ? usage / total * 100 : 0;
-  return {
-    free,
-    usage,
-    usagePercent,
-    total
-  };
-}
-
-function getMemoryUsage() {
-  const free = os.freemem();
-  const total = os.totalmem();
-  const usage = total - free;
-  const usagePercent = total ? usage / total * 100 : 0;
-  return {
-    os: {
-      free,
-      usage,
-      usagePercent,
-      total,
-    },
-    heap: process.memoryUsage(),
-    heapStats: v8.getHeapStatistics(),
-  };
-}
-
-function getDiskUsage() {
-  try {
-    const diskUsage = disk.checkSync(DISK_USAGE_PATH);
-    const free =  _.get(diskUsage, 'free', 0);
-    const total = _.get(diskUsage, 'total', 0);
-    const usage = total - free;
-    const usagePercent = total ? usage / total * 100 : 0;
-    return Object.assign({}, diskUsage, { usage, usagePercent });
-  } catch (err) {
-    logger.error(`Error: ${err} ${err.stack}`);
-    return {};
-  }
-}
-
-function getRuntimeInfo() {
-  return {
-    process: {
-      version: process.version,
-      platform: process.platform,
-      pid: process.pid,
-      uptime: Math.floor(process.uptime()),
-      v8Version: process.versions.v8,
-    },
-    os: {
-      hostname: os.hostname(),
-      type: os.type(),
-      release: os.release(),
-      // See: https://github.com/ainblockchain/ain-blockchain/issues/181
-      // version: os.version(),
-      uptime: os.uptime(),
-    },
-    env: {
-      NETWORK_OPTIMIZATION: process.env.NETWORK_OPTIMIZATION,
-      BLOCKCHAIN_CONFIGS_DIR: process.env.BLOCKCHAIN_CONFIGS_DIR,
-      MIN_NUM_VALIDATORS: process.env.MIN_NUM_VALIDATORS,
-      MAX_NUM_VALIDATORS: process.env.MAX_NUM_VALIDATORS,
-      ACCOUNT_INDEX: process.env.ACCOUNT_INDEX,
-      P2P_PORT: process.env.P2P_PORT,
-      PORT: process.env.PORT,
-      HOSTING_ENV: process.env.HOSTING_ENV,
-      DEBUG: process.env.DEBUG,
-    },
-  };
-}
-
-function getProtocolInfo() {
-  return {
-    currentVersion: BlockchainConfigs.CURRENT_PROTOCOL_VERSION,
-  };
-}
