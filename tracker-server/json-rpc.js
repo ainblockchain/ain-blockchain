459e7025e78404fdcca1fc32e1738160c045d330
minsulee2 <minsu@comcom.ai>
2021-11-26 21:21:09 +0900

    Use json-rpc to update peerNodes

--- tracker-server/json-rpc.js
+++ tracker-server/json-rpc.js
@@ -7,33 +7,40 @@
  * @return {dict} A closure of functions compatible with the jayson library for
  *                  servicing JSON-RPC requests.
  */
-module.exports = function getMethods(nodes) {
+module.exports = function getMethods(tracker) {
+  const peerNodes = tracker.peerNodes;
   return {
     getNodeInfoList: function(args, done) {
       const list = [];
-      Object.keys(nodes).forEach((key) => {
-        list.push(nodes[key].getNodeInfo());
+      Object.keys(peerNodes).forEach((key) => {
+        list.push(peerNodes[key].getNodeInfo());
       });
       done(null, list);
     },
 
     getNodeAddressList: function(args, done) {
       const list = [];
-      Object.keys(nodes).forEach((key) => {
-        list.push(nodes[key].address);
+      Object.keys(peerNodes).forEach((key) => {
+        list.push(peerNodes[key].address);
       });
       done(null, list);
     },
 
     getNodeInfoByAddress: function(args, done) {
       let result = null;
-      for (let i = 0; i < nodes.length; i++) {
-        if (nodes[i].address === args[0]) {
-          result = nodes[i].getNodeInfo();
+      for (let i = 0; i < peerNodes.length; i++) {
+        if (peerNodes[i].address === args[0]) {
+          result = peerNodes[i].getNodeInfo();
           break;
         }
       }
       done(null, result);
     },
+
+    updateNodeInfo: function(args, done) {
+      const nodeInfo = args;
+      tracker.setPeerNodes(nodeInfo);
+      done(null);
+    }
   };
 };
