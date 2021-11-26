39d660010a55b6b6717549ef834321ffd38d353b
Dongil Seo <seo@comcom.ai>
2021-11-26 16:00:42 +0900

    Apply wrapped command lines to all executions.

--- start_servers_local.sh
+++ start_servers_local.sh
@@ -2,9 +2,15 @@
 
 printf "\n[[[[[ start_servers_local.sh ]]]]]\n\n"
 
-# PARENT CHAIN
-BLOCKCHAIN_DATA_DIR=~/ain_blockchain_data node ./tracker-server/index.js &
+# PARENT CHAIN **
+# parent tracker
+printf "\nStarting parent tracker..\n"
+BLOCKCHAIN_DATA_DIR=~/ain_blockchain_data \
+  node ./tracker-server/index.js &
+printf "\nDone\n\n"
 sleep 5
+# parent node 0
+printf "\nStarting parent node 0..\n"
 ACCOUNT_INDEX=0 \
   P2P_PEER_CANDIDATE_URL='' \
   STAKE=100000 \
@@ -16,7 +22,10 @@ ACCOUNT_INDEX=0 \
   ENABLE_GAS_FEE_WORKAROUND=true \
   BLOCKCHAIN_DATA_DIR=~/ain_blockchain_data \
   node ./client/index.js &
+printf "\nDone\n\n"
 sleep 10
+# parent node 1
+printf "\nStarting parent node 1..\n"
 ACCOUNT_INDEX=1 \
   STAKE=100000 \
   CONSOLE_LOG=true \
@@ -27,7 +36,10 @@ ACCOUNT_INDEX=1 \
   ENABLE_GAS_FEE_WORKAROUND=true \
   BLOCKCHAIN_DATA_DIR=~/ain_blockchain_data \
   node ./client/index.js &
+printf "\nDone\n\n"
 sleep 10
+# parent node 2
+printf "\nStarting parent node 2..\n"
 ACCOUNT_INDEX=2 \
   STAKE=100000 \
   CONSOLE_LOG=true \
@@ -38,11 +50,21 @@ ACCOUNT_INDEX=2 \
   ENABLE_GAS_FEE_WORKAROUND=true \
   BLOCKCHAIN_DATA_DIR=~/ain_blockchain_data \
   node ./client/index.js &
+printf "\nDone\n\n"
 sleep 10
 
-# CHILD CHAIN 1
-BLOCKCHAIN_CONFIGS_DIR=blockchain-configs/afan-shard PORT=9000 P2P_PORT=6000 BLOCKCHAIN_DATA_DIR=~/ain_blockchain_data node ./tracker-server/index.js &
+# CHILD CHAIN 1 **
+# child 1 tracker
+printf "\nStarting child 1 tracker..\n"
+BLOCKCHAIN_CONFIGS_DIR=blockchain-configs/afan-shard \
+  PORT=9000 \
+  P2P_PORT=6000 \
+  BLOCKCHAIN_DATA_DIR=~/ain_blockchain_data \
+  node ./tracker-server/index.js &
+printf "\nDone\n\n"
 sleep 10
+# child 1 node 0
+printf "\nStarting child 1 node 0..\n"
 BLOCKCHAIN_CONFIGS_DIR=blockchain-configs/afan-shard \
   PORT=9001 \
   P2P_PORT=6001 \
@@ -57,6 +79,7 @@ BLOCKCHAIN_CONFIGS_DIR=blockchain-configs/afan-shard \
   ENABLE_GAS_FEE_WORKAROUND=true \
   BLOCKCHAIN_DATA_DIR=~/ain_blockchain_data \
   node ./client/index.js &
+printf "\nDone\n\n"
 sleep 10
 
 while :
@@ -70,8 +93,25 @@ do
     sleep 20
 done
 
-BLOCKCHAIN_CONFIGS_DIR=blockchain-configs/afan-shard PORT=9002 P2P_PORT=6002 ACCOUNT_INDEX=1 STAKE=100000 CONSOLE_LOG=true ENABLE_DEV_CLIENT_SET_API=true ENABLE_JSON_RPC_TX_API=true ENABLE_REST_FUNCTION_CALL=true ENABLE_TX_SIG_VERIF_WORKAROUND=true ENABLE_GAS_FEE_WORKAROUND=true BLOCKCHAIN_DATA_DIR=~/ain_blockchain_data node ./client/index.js &
+# child 1 node 1
+printf "\nStarting child 1 node 1..\n"
+BLOCKCHAIN_CONFIGS_DIR=blockchain-configs/afan-shard \
+  PORT=9002 \
+  P2P_PORT=6002 \
+  ACCOUNT_INDEX=1 \
+  STAKE=100000 \
+  CONSOLE_LOG=true \
+  ENABLE_DEV_CLIENT_SET_API=true \
+  ENABLE_JSON_RPC_TX_API=true \
+  ENABLE_REST_FUNCTION_CALL=true \
+  ENABLE_TX_SIG_VERIF_WORKAROUND=true \
+  ENABLE_GAS_FEE_WORKAROUND=true \
+  BLOCKCHAIN_DATA_DIR=~/ain_blockchain_data \
+  node ./client/index.js &
+printf "\nDone\n\n"
 sleep 10
+# child 1 node 2
+printf "\nStarting child 1 node 2..\n"
 BLOCKCHAIN_CONFIGS_DIR=blockchain-configs/afan-shard \
   PORT=9003 \
   P2P_PORT=6003 \
@@ -85,11 +125,21 @@ BLOCKCHAIN_CONFIGS_DIR=blockchain-configs/afan-shard \
   ENABLE_GAS_FEE_WORKAROUND=true \
   BLOCKCHAIN_DATA_DIR=~/ain_blockchain_data \
   node ./client/index.js &
+printf "\nDone\n\n"
 sleep 10
 
-# CHILD CHAIN 2
-BLOCKCHAIN_CONFIGS_DIR=blockchain-configs/sim-shard PORT=9010 P2P_PORT=6010 BLOCKCHAIN_DATA_DIR=~/ain_blockchain_data node ./tracker-server/index.js &
+# CHILD CHAIN 2 **
+# child 2 tracker
+printf "\nStarting child 2 tracker..\n"
+BLOCKCHAIN_CONFIGS_DIR=blockchain-configs/sim-shard \
+  PORT=9010 \
+  P2P_PORT=6010 \
+  BLOCKCHAIN_DATA_DIR=~/ain_blockchain_data \
+  node ./tracker-server/index.js &
+printf "\nDone\n\n"
 sleep 10
+# child 2 node 0
+printf "\nStarting child 2 node 0..\n"
 BLOCKCHAIN_CONFIGS_DIR=blockchain-configs/sim-shard \
   PORT=9011 \
   P2P_PORT=6011 \
@@ -103,6 +153,7 @@ BLOCKCHAIN_CONFIGS_DIR=blockchain-configs/sim-shard \
   ENABLE_GAS_FEE_WORKAROUND=true \
   BLOCKCHAIN_DATA_DIR=~/ain_blockchain_data \
   node ./client/index.js &
+printf "\nDone\n\n"
 sleep 10
 
 while :
@@ -116,8 +167,25 @@ do
     sleep 20
 done
 
-BLOCKCHAIN_CONFIGS_DIR=blockchain-configs/sim-shard PORT=9012 P2P_PORT=6012 ACCOUNT_INDEX=1 STAKE=100000 CONSOLE_LOG=true ENABLE_DEV_CLIENT_SET_API=true ENABLE_JSON_RPC_TX_API=true ENABLE_REST_FUNCTION_CALL=true ENABLE_TX_SIG_VERIF_WORKAROUND=true ENABLE_GAS_FEE_WORKAROUND=true BLOCKCHAIN_DATA_DIR=~/ain_blockchain_data node ./client/index.js &
+# child 2 node 1
+printf "\nStarting child 2 node 1..\n"
+BLOCKCHAIN_CONFIGS_DIR=blockchain-configs/sim-shard \
+  PORT=9012 \
+  P2P_PORT=6012 \
+  ACCOUNT_INDEX=1 \
+  STAKE=100000 \
+  CONSOLE_LOG=true \
+  ENABLE_DEV_CLIENT_SET_API=true \
+  ENABLE_JSON_RPC_TX_API=true \
+  ENABLE_REST_FUNCTION_CALL=true \
+  ENABLE_TX_SIG_VERIF_WORKAROUND=true \
+  ENABLE_GAS_FEE_WORKAROUND=true \
+  BLOCKCHAIN_DATA_DIR=~/ain_blockchain_data \
+  node ./client/index.js &
+printf "\nDone\n\n"
 sleep 10
+# child 2 node 2
+printf "\nStarting child 2 node 2..\n"
 BLOCKCHAIN_CONFIGS_DIR=blockchain-configs/sim-shard \
   PORT=9013 \
   P2P_PORT=6013 \
@@ -131,4 +199,5 @@ BLOCKCHAIN_CONFIGS_DIR=blockchain-configs/sim-shard \
   ENABLE_GAS_FEE_WORKAROUND=true \
   BLOCKCHAIN_DATA_DIR=~/ain_blockchain_data \
   node ./client/index.js &
+printf "\nDone\n\n"
 sleep 10
