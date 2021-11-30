087dfa675de2b32d57570c02057881f37099ac45
Dongil Seo <seo@comcom.ai>
2021-11-30 23:20:45 +0900

    Update tests.

--- unittest/traffic-stats-manager.test.js
+++ unittest/traffic-stats-manager.test.js
@@ -22,122 +22,185 @@ describe("traffic-stats-manager", () => {
     });
   });
 
-  describe("addEvent / countEvents", () => {
+  describe("addEvent / getEventSums", () => {
     it("with intervals not overlapping", () => {
       expect(tm.eventCounterMap.size).to.equal(0);
 
-      tm.addEvent(eventType1, initialTimeMs);
+      tm.addEvent(eventType1, 10, initialTimeMs);
       expect(tm.eventCounterMap.size).to.equal(1);
       expect(tm.eventCounterMap.has(eventType1)).to.equal(true);
-      expect(tm.countEvents(eventType1, intervalMs * 10, initialTimeMs + intervalMs)).to.equal(1);
-      expect(tm.countEvents(eventType2, intervalMs * 10, initialTimeMs + intervalMs)).to.equal(0);
+      assert.deepEqual(tm.getEventSums(eventType1, intervalMs * 10, initialTimeMs + intervalMs), {
+        countSum: 1,
+        latencySum: 10,
+      });
+      assert.deepEqual(tm.getEventSums(eventType2, intervalMs * 10, initialTimeMs + intervalMs), null);
 
-      tm.addEvent(eventType2, initialTimeMs + intervalMs);
+      tm.addEvent(eventType2, 100, initialTimeMs + intervalMs);
       expect(tm.eventCounterMap.size).to.equal(2);
       expect(tm.eventCounterMap.has(eventType1)).to.equal(true);
       expect(tm.eventCounterMap.has(eventType2)).to.equal(true);
-      expect(tm.countEvents(eventType1, intervalMs * 10, initialTimeMs + intervalMs * 2)).to.equal(1);
-      expect(tm.countEvents(eventType2, intervalMs * 10, initialTimeMs + intervalMs * 2)).to.equal(1);
+      assert.deepEqual(tm.getEventSums(eventType1, intervalMs * 10, initialTimeMs + intervalMs * 2), {
+        countSum: 1,
+        latencySum: 10,
+      });
+      assert.deepEqual(tm.getEventSums(eventType2, intervalMs * 10, initialTimeMs + intervalMs * 2), {
+        countSum: 1,
+        latencySum: 100,
+      });
 
-      tm.addEvent(eventType1, initialTimeMs + intervalMs * 2);
-      tm.addEvent(eventType2, initialTimeMs + intervalMs * 2);
-      tm.addEvent(eventType1, initialTimeMs + intervalMs * 2);
-      expect(tm.countEvents(eventType1, intervalMs * 10, initialTimeMs + intervalMs * 3)).to.equal(3);
-      expect(tm.countEvents(eventType2, intervalMs * 10, initialTimeMs + intervalMs * 3)).to.equal(2);
+      tm.addEvent(eventType1, 10, initialTimeMs + intervalMs * 2);
+      tm.addEvent(eventType2, 100, initialTimeMs + intervalMs * 2);
+      tm.addEvent(eventType1, 10, initialTimeMs + intervalMs * 2);
+      assert.deepEqual(tm.getEventSums(eventType1, intervalMs * 10, initialTimeMs + intervalMs * 3), {
+        countSum: 3,
+        latencySum: 30,
+      });
+      assert.deepEqual(tm.getEventSums(eventType2, intervalMs * 10, initialTimeMs + intervalMs * 3), {
+        countSum: 2,
+        latencySum: 200,
+      });
     });
 
     it("with intervals overlapping", () => {
       expect(tm.eventCounterMap.size).to.equal(0);
 
-      tm.addEvent(eventType1, initialTimeMs);
-      tm.addEvent(eventType1, initialTimeMs);
-      tm.addEvent(eventType1, initialTimeMs);
-      tm.addEvent(eventType2, initialTimeMs);
-      tm.addEvent(eventType2, initialTimeMs);
+      tm.addEvent(eventType1, 10, initialTimeMs);
+      tm.addEvent(eventType1, 10, initialTimeMs);
+      tm.addEvent(eventType1, 10, initialTimeMs);
+      tm.addEvent(eventType2, 100, initialTimeMs);
+      tm.addEvent(eventType2, 100, initialTimeMs);
       // numIntervals = 10, lastQueueIndex = 0
-      expect(tm.countEvents(eventType1, intervalMs * maxIntervals, initialTimeMs)).to.equal(3);
-      expect(tm.countEvents(eventType2, intervalMs * maxIntervals, initialTimeMs)).to.equal(2);
+      assert.deepEqual(tm.getEventSums(eventType1, intervalMs * maxIntervals, initialTimeMs), {
+        countSum: 3,
+        latencySum: 30,
+      });
+      assert.deepEqual(tm.getEventSums(eventType2, intervalMs * maxIntervals, initialTimeMs), {
+        countSum: 2,
+        latencySum: 200,
+      });
 
       // overlapping of 100 rounds
-      tm.addEvent(eventType1, initialTimeMs + intervalMs * (maxIntervals * 100 + 4));
-      tm.addEvent(eventType1, initialTimeMs + intervalMs * (maxIntervals * 100 + 4));
-      tm.addEvent(eventType2, initialTimeMs + intervalMs * (maxIntervals * 100 + 4));
+      tm.addEvent(eventType1, 10, initialTimeMs + intervalMs * (maxIntervals * 100 + 4));
+      tm.addEvent(eventType1, 10, initialTimeMs + intervalMs * (maxIntervals * 100 + 4));
+      tm.addEvent(eventType2, 100, initialTimeMs + intervalMs * (maxIntervals * 100 + 4));
       // numIntervals = 10, lastQueueIndex = 0
-      expect(tm.countEvents(eventType1, intervalMs * maxIntervals, initialTimeMs)).to.equal(2);
-      expect(tm.countEvents(eventType2, intervalMs * maxIntervals, initialTimeMs)).to.equal(1);
+      assert.deepEqual(tm.getEventSums(eventType1, intervalMs * maxIntervals, initialTimeMs), {
+        countSum: 2,
+        latencySum: 20,
+      });
+      assert.deepEqual(tm.getEventSums(eventType2, intervalMs * maxIntervals, initialTimeMs), {
+        countSum: 1,
+        latencySum: 100,
+      });
     });
   });
 
-  describe("getEventRates", () => {
+  describe("getEventStats", () => {
     beforeEach(() => {
       expect(tm.eventCounterMap.size).to.equal(0);
-      tm.addEvent(eventType1, initialTimeMs);
-      tm.addEvent(eventType1, initialTimeMs);
-      tm.addEvent(eventType1, initialTimeMs);
-      tm.addEvent(eventType1, initialTimeMs);
-      tm.addEvent(eventType1, initialTimeMs);
-      tm.addEvent(eventType2, initialTimeMs);
-      tm.addEvent(eventType2, initialTimeMs);
-      tm.addEvent(eventType2, initialTimeMs);
-      tm.addEvent(eventType2, initialTimeMs);
-      tm.addEvent(eventType2, initialTimeMs);
+      tm.addEvent(eventType1, 10, initialTimeMs);
+      tm.addEvent(eventType1, 10, initialTimeMs);
+      tm.addEvent(eventType1, 10, initialTimeMs);
+      tm.addEvent(eventType1, 10, initialTimeMs);
+      tm.addEvent(eventType1, 10, initialTimeMs);
+      tm.addEvent(eventType2, 100, initialTimeMs);
+      tm.addEvent(eventType2, 100, initialTimeMs);
+      tm.addEvent(eventType2, 100, initialTimeMs);
+      tm.addEvent(eventType2, 100, initialTimeMs);
+      tm.addEvent(eventType2, 100, initialTimeMs);
 
-      tm.addEvent(eventType1, initialTimeMs + intervalMs);
-      tm.addEvent(eventType1, initialTimeMs + intervalMs);
-      tm.addEvent(eventType1, initialTimeMs + intervalMs);
-      tm.addEvent(eventType1, initialTimeMs + intervalMs);
-      tm.addEvent(eventType1, initialTimeMs + intervalMs);
-      tm.addEvent(eventType2, initialTimeMs + intervalMs);
-      tm.addEvent(eventType2, initialTimeMs + intervalMs);
-      tm.addEvent(eventType2, initialTimeMs + intervalMs);
-      tm.addEvent(eventType2, initialTimeMs + intervalMs);
-      tm.addEvent(eventType2, initialTimeMs + intervalMs);
+      tm.addEvent(eventType1, 10, initialTimeMs + intervalMs);
+      tm.addEvent(eventType1, 10, initialTimeMs + intervalMs);
+      tm.addEvent(eventType1, 10, initialTimeMs + intervalMs);
+      tm.addEvent(eventType1, 10, initialTimeMs + intervalMs);
+      tm.addEvent(eventType1, 10, initialTimeMs + intervalMs);
+      tm.addEvent(eventType2, 100, initialTimeMs + intervalMs);
+      tm.addEvent(eventType2, 100, initialTimeMs + intervalMs);
+      tm.addEvent(eventType2, 100, initialTimeMs + intervalMs);
+      tm.addEvent(eventType2, 100, initialTimeMs + intervalMs);
+      tm.addEvent(eventType2, 100, initialTimeMs + intervalMs);
 
-      tm.addEvent(eventType1, initialTimeMs + intervalMs * 3);
-      tm.addEvent(eventType1, initialTimeMs + intervalMs * 3);
-      tm.addEvent(eventType1, initialTimeMs + intervalMs * 3);
-      tm.addEvent(eventType1, initialTimeMs + intervalMs * 3);
-      tm.addEvent(eventType1, initialTimeMs + intervalMs * 3);
-      tm.addEvent(eventType2, initialTimeMs + intervalMs * 3);
-      tm.addEvent(eventType2, initialTimeMs + intervalMs * 3);
-      tm.addEvent(eventType2, initialTimeMs + intervalMs * 3);
-      tm.addEvent(eventType2, initialTimeMs + intervalMs * 3);
-      tm.addEvent(eventType2, initialTimeMs + intervalMs * 3);
+      tm.addEvent(eventType1, 10, initialTimeMs + intervalMs * 3);
+      tm.addEvent(eventType1, 10, initialTimeMs + intervalMs * 3);
+      tm.addEvent(eventType1, 10, initialTimeMs + intervalMs * 3);
+      tm.addEvent(eventType1, 10, initialTimeMs + intervalMs * 3);
+      tm.addEvent(eventType1, 10, initialTimeMs + intervalMs * 3);
+      tm.addEvent(eventType2, 100, initialTimeMs + intervalMs * 3);
+      tm.addEvent(eventType2, 100, initialTimeMs + intervalMs * 3);
+      tm.addEvent(eventType2, 100, initialTimeMs + intervalMs * 3);
+      tm.addEvent(eventType2, 100, initialTimeMs + intervalMs * 3);
+      tm.addEvent(eventType2, 100, initialTimeMs + intervalMs * 3);
     })
 
     it("with invalid periods", () => {
       // with invalid interval period
-      assert.deepEqual(tm.getEventRates(0, initialTimeMs + intervalMs * 4), {
-        "event_type1": -1,
-        "event_type2": -1,
+      assert.deepEqual(tm.getEventStats(0, initialTimeMs + intervalMs * 4), {
+        "event_type1": {
+          "rate": 0,
+          "latency": 0,
+        },
+        "event_type2": {
+          "rate": 0,
+          "latency": 0,
+        },
       })
       // with 11 interval period
-      assert.deepEqual(tm.getEventRates(intervalMs / 1000 * 11, initialTimeMs + intervalMs * 4), {
-        "event_type1": -1,
-        "event_type2": -1,
+      assert.deepEqual(tm.getEventStats(intervalMs / 1000 * 11, initialTimeMs + intervalMs * 4), {
+        "event_type1": {
+          "rate": 0,
+          "latency": 0,
+        },
+        "event_type2": {
+          "rate": 0,
+          "latency": 0,
+        },
       })
     });
 
     it("with valid periods", () => {
       // with 1 interval period
-      assert.deepEqual(tm.getEventRates(intervalMs / 1000, initialTimeMs + intervalMs * 4), {
-        "event_type1": 5,
-        "event_type2": 5,
+      assert.deepEqual(tm.getEventStats(intervalMs / 1000, initialTimeMs + intervalMs * 4), {
+        "event_type1": {
+          "rate": 5,
+          "latency": 10,
+        },
+        "event_type2": {
+          "rate": 5,
+          "latency": 100,
+        },
       })
       // with 2 interval period
-      assert.deepEqual(tm.getEventRates(intervalMs / 1000 * 2, initialTimeMs + intervalMs * 4), {
-        "event_type1": 2.5,
-        "event_type2": 2.5,
+      assert.deepEqual(tm.getEventStats(intervalMs / 1000 * 2, initialTimeMs + intervalMs * 4), {
+        "event_type1": {
+          "rate": 2.5,
+          "latency": 10,
+        },
+        "event_type2": {
+          "rate": 2.5,
+          "latency": 100,
+        },
       })
       // with 3 interval period
-      assert.deepEqual(tm.getEventRates(intervalMs / 1000 * 3, initialTimeMs + intervalMs * 4), {
-        "event_type1": 3.3333333333333335,
-        "event_type2": 3.3333333333333335,
+      assert.deepEqual(tm.getEventStats(intervalMs / 1000 * 3, initialTimeMs + intervalMs * 4), {
+        "event_type1": {
+          "rate": 3.3333333333333335,
+          "latency": 10,
+        },
+        "event_type2": {
+          "rate": 3.3333333333333335,
+          "latency": 100,
+        },
       })
       // with 10 interval period
-      assert.deepEqual(tm.getEventRates(intervalMs / 1000 * 10, initialTimeMs + intervalMs * 4), {
-        "event_type1": 1.5,
-        "event_type2": 1.5,
+      assert.deepEqual(tm.getEventStats(intervalMs / 1000 * 10, initialTimeMs + intervalMs * 4), {
+        "event_type1": {
+          "rate": 1.5,
+          "latency": 10,
+        },
+        "event_type2": {
+          "rate": 1.5,
+          "latency": 100,
+        },
       })
     });
   });
