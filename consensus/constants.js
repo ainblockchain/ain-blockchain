0a53b8fdd80f7a28e0e11c0e3761d5b92453752b
Lia Yoo <lia@comcom.ai>
2021-12-29 18:23:43 +0900

    Move some of the consensus consts to blockchain params & node params.

--- consensus/constants.js
+++ consensus/constants.js
@@ -1,10 +1,6 @@
 const ConsensusConsts = {
   MAJORITY: 2 / 3,
-  DAY_MS: 86400000,
-  MAX_INVALID_BLOCKS_ON_MEM: 100,
-  MAX_FINALIZED_BLOCK_INFO_ON_MEM: 10000,
   HEALTH_THRESHOLD_EPOCH: 10,
-  STAKE_LOCKUP_EXTENSION: 2592000000, // 30 days as ms
 };
 
 const ConsensusMessageTypes = {
