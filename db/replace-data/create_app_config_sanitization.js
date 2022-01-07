61e8558861746171b7b0f08c9994560445248b5d
Lia Yoo <lia@comcom.ai>
2022-01-07 20:41:16 +0900

    Add prevValue in bandage files for future reference.

--- db/replace-data/create_app_config_sanitization.js
+++ db/replace-data/create_app_config_sanitization.js
@@ -1,36 +1,48 @@
 module.exports = {
   data: [
     {
-      path: ['rules', 'manage_app'],
+      path: ['rules', 'manage_app', '$app_name'],
       value: {
-        '$app_name': {
-          'config': {
-            'admin': {
-              '.rule': {
-                'write': `(auth.fid === \'_createApp\' || util.isAppAdmin($app_name, auth.addr, getValue) === true) && util.validateManageAppAdminConfig(newData)`
-              }
-            },
-            'billing': {
-              '.rule': {
-                'write': `(auth.fid === \'_createApp\' || util.isAppAdmin($app_name, auth.addr, getValue) === true) && util.validateManageAppBillingConfig(newData)`
-              }
-            },
-            'is_public': {
-              '.rule': {
-                'write': `(auth.fid === \'_createApp\' || util.isAppAdmin($app_name, auth.addr, getValue) === true) && util.validateManageAppIsPublicConfig(newData)`
-              }
-            },
-            'service': {
-              '.rule': {
-                'write': `(auth.fid === \'_createApp\' || util.isAppAdmin($app_name, auth.addr, getValue) === true) && util.validateManageAppServiceConfig(newData)`
-              }
+        'config': {
+          'admin': {
+            '.rule': {
+              'write': `(auth.fid === \'_createApp\' || util.isAppAdmin($app_name, auth.addr, getValue) === true) && util.validateManageAppAdminConfig(newData)`
             }
           },
-          'create': {
-            '$record_id': {
-              '.rule': {
-                'write': `data === null && getValue(\'/manage_app/\' + $app_name + \'/config\') === null && util.isDict(newData)`
-              }
+          'billing': {
+            '.rule': {
+              'write': `(auth.fid === \'_createApp\' || util.isAppAdmin($app_name, auth.addr, getValue) === true) && util.validateManageAppBillingConfig(newData)`
+            }
+          },
+          'is_public': {
+            '.rule': {
+              'write': `(auth.fid === \'_createApp\' || util.isAppAdmin($app_name, auth.addr, getValue) === true) && util.validateManageAppIsPublicConfig(newData)`
+            }
+          },
+          'service': {
+            '.rule': {
+              'write': `(auth.fid === \'_createApp\' || util.isAppAdmin($app_name, auth.addr, getValue) === true) && util.validateManageAppServiceConfig(newData)`
+            }
+          }
+        },
+        'create': {
+          '$record_id': {
+            '.rule': {
+              'write': `data === null && getValue(\'/manage_app/\' + $app_name + \'/config\') === null && util.isDict(newData)`
+            }
+          }
+        }
+      },
+      prevValue: {
+        'config': {
+          '.rule': {
+            'write': 'auth.fid === \'_createApp\' || util.isAppAdmin($app_name, auth.addr, getValue) === true'
+          }
+        },
+        'create': {
+          '$record_id': {
+            '.rule': {
+              'write': 'data === null && getValue(\'/manage_app/\' + $app_name + \'/config\') === null && util.isDict(newData)'
             }
           }
         }
