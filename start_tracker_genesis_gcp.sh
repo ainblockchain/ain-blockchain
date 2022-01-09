3a202fc0bc546c63ec195fe5c598cae0d64b383f
Dongil Seo <seo@comcom.ai>
2022-01-09 17:19:34 +0900

    Use ~/ain-blockchain instead of ~ directory for deployment

--- start_tracker_genesis_gcp.sh
+++ start_tracker_genesis_gcp.sh
@@ -31,31 +31,30 @@ killall node
 if [[ $KEEP_CODE_OPTION = "--no-keep-code" ]]; then
     printf '\n'
     printf 'Creating new working directory..\n'
-    cd
-    sudo rm -rf /home/ain_blockchain_data
-    sudo mkdir /home/ain_blockchain_data
-    sudo chmod -R 777 /home/ain_blockchain_data
-    sudo rm -rf ../ain-blockchain*
-    sudo mkdir ../ain-blockchain
-    sudo chmod -R 777 ../ain-blockchain
-    mv * ../ain-blockchain
-    cd ../ain-blockchain
+    sudo rm -rf /home/ain-blockchain*
+    CODE_CMD="cd ~; sudo mv ain-blockchain /home; sudo chmod -R 777 /home/ain-blockchain; sudo chown -R root:root /home/ain-blockchain; cd /home/ain-blockchain"
+    printf "\nCODE_CMD=$CODE_CMD\n"
+    eval $CODE_CMD
 
     printf '\n'
     printf 'Installing node modules..\n'
-    sudo yarn install --ignore-engines
+    INSTALL_CMD="sudo yarn install --ignore-engines"
+    printf "\nINSTALL_CMD=$INSTALL_CMD\n"
+    eval $INSTALL_CMD
 else
     printf '\n'
     printf 'Using old directory..\n'
-    OLD_DIR_PATH=$(find ../ain-blockchain* -maxdepth 0 -type d)
+    OLD_DIR_PATH=$(find /home/ain-blockchain* -maxdepth 0 -type d)
     printf "OLD_DIR_PATH=$OLD_DIR_PATH\n"
-    sudo chmod -R 777 $OLD_DIR_PATH
+    CODE_CMD="sudo chmod -R 777 $OLD_DIR_PATH; sudo chown -R root:root $OLD_DIR_PATH"
+    printf "\nCODE_CMD=$CODE_CMD\n"
+    eval $CODE_CMD
 fi
 
 
 printf "\nStarting up Blockchain Tracker server..\n\n"
 START_CMD="nohup node --async-stack-traces tracker-server/index.js >/dev/null 2>error_logs.txt &"
-printf "START_CMD=$START_CMD\n"
+printf "\nSTART_CMD=$START_CMD\n"
 printf "START_CMD=$START_CMD\n" >> start_commands.txt
 eval $START_CMD
 
