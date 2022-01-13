564509ed38fcd0770271607c88d50d2d3806f578
Dongil Seo <seo@comcom.ai>
2022-01-13 21:17:57 +0900

    Update max_state_tree_size_per_byte with 1/160

---
+++ db/bandage-files/update_max_state_tree_size_per_byte2.js
@@ -0,0 +1,9 @@
+module.exports = {
+  data: [
+    {
+      path: ['values', 'blockchain_params', 'resource', 'max_state_tree_size_per_byte'],
+      value: 0.00625,
+      prevValue: 0.1
+    }
+  ]
+};
