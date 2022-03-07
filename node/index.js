81b4f075d5d793f63ccc62921aa302f98a1f0aa4
liayoo <lia@comcom.ai>
2022-03-07 13:54:58 +0900

    Add REVERTED state to TransactionStates.

--- node/index.js
+++ node/index.js
@@ -16,6 +16,7 @@ const {
   ShardingProperties,
   ShardingProtocols,
   TransactionStates,
+  isTxInBlock,
   StateVersions,
   SyncModeOptions,
   WriteDbOperations,
@@ -451,7 +452,7 @@ class BlockchainNode {
       return null;
     }
 
-    if (transactionInfo.state === TransactionStates.FINALIZED) {
+    if (isTxInBlock(transactionInfo.state)) {
       const block = this.bc.getBlockByNumber(transactionInfo.number);
       const index = transactionInfo.index;
       if (!block) {
