8c7568c27949fe1065a58e2b74f1dc3b5c5227cd
minsulee2 <minsu@comcom.ai>
2022-04-12 18:21:36 +0900

    Create app with chainId as argv

--- tools/he-health-care/sendCreateAppTx.js
+++ tools/he-health-care/sendCreateAppTx.js
@@ -18,10 +18,21 @@ function buildCreateAppTxBody(appName, timestamp) {
   };
 }
 
+function usage() {
+  console.log('\nExample commandlines:\n  node sendCreateAppTx.js chainId\n');
+  process.exit(0);
+}
+
 async function main() {
-  // TODO(cshcomcom): Support 'node sendCreateAppTx.js <config_filename>' and check args
+  // TODO(cshcomcom): Support 'node sendCreateAppTx.js chainId <config_filename>' and check args
+  if (process.argv.length !== 2 && process.argv.length !== 3) {
+    usage();
+    process.exit(0);
+  }
+  const chainId = process.argv[2];
   const createHealthCareAppTxBody = buildCreateAppTxBody(healthCareAppName, Date.now());
-  const createResult = await signAndSendTx(endpointUrl, createHealthCareAppTxBody, serviceOwnerPrivateKey);
+  const createResult =
+      await signAndSendTx(endpointUrl, createHealthCareAppTxBody, serviceOwnerPrivateKey, chainId);
   if (!createResult.success) {
     throw Error(`Can't create health care app (${JSON.stringify(createResult, null, 2)})`);
   }
