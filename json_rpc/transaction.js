d7173bb0d3385035ccc6b5fc00032d85f788ef03
Dongil Seo <seo@comcom.ai>
2022-04-20 15:52:39 +0900

    Early verify tx signature

--- json_rpc/transaction.js
+++ json_rpc/transaction.js
@@ -1,6 +1,7 @@
 const sizeof = require('object-sizeof');
 const JsonRpcUtil = require('./json-rpc-util');
 const {
+  NodeConfigs,
   TrafficEventTypes,
   trafficStatsManager,
 } = require('../common/constants');
@@ -107,6 +108,16 @@ module.exports = function getTransactionApis(node, p2pServer) {
             }
           }));
         } else {
+          if (!NodeConfigs.LIGHTWEIGHT) {
+            if (!Transaction.verifyTransaction(createdTx, chainId)) {
+              done(null, JsonRpcUtil.addProtocolVersion({
+                result: {
+                  code: JsonRpcApiResultCode.TX_INVALID_SIGNATURE,
+                  message: `Invalid transaction signature.`
+                }
+              }));
+            }
+          }
           const result = p2pServer.executeAndBroadcastTransaction(createdTx);
           const latency = Date.now() - beginTime;
           trafficStatsManager.addEvent(TrafficEventTypes.JSON_RPC_SET, latency);
@@ -174,6 +185,16 @@ module.exports = function getTransactionApis(node, p2pServer) {
               }
             }));
             return;
+          }
+          if (!NodeConfigs.LIGHTWEIGHT) {
+            if (!Transaction.verifyTransaction(createdTx, chainId)) {
+              done(null, JsonRpcUtil.addProtocolVersion({
+                result: {
+                  code: JsonRpcApiResultCode.BATCH_TX_INVALID_SIGNATURE,
+                  message: `Invalid signature of transaction[${i}].`
+                }
+              }));
+            }
           }
           txList.push(createdTx);
         }
