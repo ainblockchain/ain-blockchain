048513fbc32781e3daafa3aa4afc9b216541c5db
Dongil Seo <seo@comcom.ai>
2022-04-21 15:20:47 +0900

    Verify txs from p2p network

--- json_rpc/transaction.js
+++ json_rpc/transaction.js
@@ -108,15 +108,13 @@ module.exports = function getTransactionApis(node, p2pServer) {
             }
           }));
         } else {
-          if (!NodeConfigs.LIGHTWEIGHT) {
-            if (!Transaction.verifyTransaction(createdTx, chainId)) {
-              done(null, JsonRpcUtil.addProtocolVersion({
-                result: {
-                  code: JsonRpcApiResultCode.TX_INVALID_SIGNATURE,
-                  message: `Invalid transaction signature.`
-                }
-              }));
-            }
+          if (!NodeConfigs.LIGHTWEIGHT && !Transaction.verifyTransaction(createdTx, chainId)) {
+            done(null, JsonRpcUtil.addProtocolVersion({
+              result: {
+                code: JsonRpcApiResultCode.TX_INVALID_SIGNATURE,
+                message: `Invalid transaction signature.`
+              }
+            }));
           }
           const result = p2pServer.executeAndBroadcastTransaction(createdTx);
           const latency = Date.now() - beginTime;
@@ -186,15 +184,14 @@ module.exports = function getTransactionApis(node, p2pServer) {
             }));
             return;
           }
-          if (!NodeConfigs.LIGHTWEIGHT) {
-            if (!Transaction.verifyTransaction(createdTx, chainId)) {
-              done(null, JsonRpcUtil.addProtocolVersion({
-                result: {
-                  code: JsonRpcApiResultCode.BATCH_TX_INVALID_SIGNATURE,
-                  message: `Invalid signature of transaction[${i}].`
-                }
-              }));
-            }
+          if (!NodeConfigs.LIGHTWEIGHT && !Transaction.verifyTransaction(createdTx, chainId)) {
+            done(null, JsonRpcUtil.addProtocolVersion({
+              result: {
+                code: JsonRpcApiResultCode.BATCH_TX_INVALID_SIGNATURE,
+                message: `Invalid signature of transaction[${i}].`
+              }
+            }));
+            return;
           }
           txList.push(createdTx);
         }
