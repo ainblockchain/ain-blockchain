e6b1692eddad041a6bcd8a4bbecffaa5fba5e450
minsulee2 <minsu@comcom.ai>
2022-05-09 14:40:06 +0900

    Update Middleware

--- client/middleware.js
+++ client/middleware.js
@@ -1,14 +1,49 @@
 const rateLimit = require('express-rate-limit');
 
+const { NodeConfigs } = require('../common/constants');
+
 class Middleware {
   constructor () {
+    this.readRateLimit = this.setReadRateLimit();
+    this.writeRateLimit = this.setWriteRateLimit();
   }
 
-  limiter() {
-    return rateLimit({
-      windowMs: 6 * 1000, // 1 minute
-      max: 6 // limit each IP to 60 requests per windowMs
-    });
+  setReadRateLimit() {
+    this.readRateLimit = NodeConfigs.MAX_READ_RATE_LIMIT;
+    return this;
+  }
+
+  setWriteRateLimit() {
+    this.writeRateLimit = NodeConfigs.MAX_WRITE_RATE_LIMIT;
+    return this;
+  }
+
+  getReadRateLimit() {
+    return this.readRateLimit;
+  }
+
+  getWriteRateLimit() {
+    return this.writeRateLimit;
+  }
+
+  emptyHandler(req, res, next) {
+    return next();
+  }
+
+  readLimiter() {
+    return NodeConfigs.ENABLE_EXPRESS_RATE_LIMIT ?
+        rateLimit({
+          windowMs: 1000,   // 1 second
+          max: this.getReadRateLimit()   // limit each IP to maximum of read rate limit
+        }) : this.emptyHandler();
+  }
+
+  writeLimiter() {
+    return NodeConfigs.ENABLE_EXPRESS_RATE_LIMIT ?
+        rateLimit({
+          windowMs: 1000,   // 1 second
+          max: this.getWriteRateLimit()   // limit each IP to maximum of write rate limit
+        }) : this.emptyHandler();
   }
 }
 
