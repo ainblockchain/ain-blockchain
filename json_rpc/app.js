e423e52feb3f86b91d0d2cbf401737a4c077341b
minsulee2 <minsu@comcom.ai>
2022-05-11 14:58:23 +0900

    Update json rpc method for app

--- json_rpc/app.js
+++ json_rpc/app.js
@@ -3,10 +3,11 @@ const {
   trafficStatsManager,
 } = require('../common/constants');
 const JsonRpcUtil = require('./json-rpc-util');
+const { JSON_RPC_METHOD } = require('./constants');
 
 module.exports = function getAppApis(node) {
   return {
-    ain_validateAppName: function(args, done) {
+    [JSON_RPC_METHOD.AIN_VALIDATE_APP_NAME]: function(args, done) {
       const beginTime = Date.now();
       const result = node.validateAppName(args.app_name);
       const latency = Date.now() - beginTime;
