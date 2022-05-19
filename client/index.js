524a05e4ac5d2c8ecee69281dffcd4c69ca7fdc2
minsulee2 <minsu@comcom.ai>
2022-05-19 17:34:56 +0900

    Simplify middleware.js

--- client/index.js
+++ client/index.js
@@ -32,14 +32,14 @@ app.use(middleware.expressJsonRequestBodySizeLimiter());
 app.use(middleware.expressUrlencdedRequestBodySizeLimiter());
 app.use(middleware.corsLimiter());
 app.use(middleware.ipWhitelistLimiter());
-app.use(middleware.blockchainApiLimiter());
+app.use(middleware.blockchainApiRateLimiter);
 
 const eventHandler = NodeConfigs.ENABLE_EVENT_HANDLER === true ? new EventHandler() : null;
 const node = new BlockchainNode(null, eventHandler);
 // NOTE(platfowner): This is very useful when the server dies without any logs.
-// process.on('uncaughtException', function(err) {
-//   logger.error(err);
-// });
+process.on('uncaughtException', function(err) {
+  logger.error(err);
+});
 
 process.on('SIGINT', (_) => {
   logger.info('Stopping the blockchain client....');
