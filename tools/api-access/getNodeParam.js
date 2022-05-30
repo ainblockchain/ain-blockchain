e49ac46b40a22c8c93a50d37b058ed2843bce768
kriii <kriiivv@gmail.com>
2022-05-31 02:03:28 +0900

    Add tools for new APIs

---
+++ tools/api-access/getNodeParam.js
@@ -0,0 +1,66 @@
+const axios = require('axios');
+const _ = require('lodash');
+const ainUtil = require('@ainblockchain/ain-util');
+const stringify = require('fast-json-stable-stringify');
+const { BlockchainConsts } = require('../../common/constants');
+const { getAccountPrivateKey } = require('./util');
+const { JSON_RPC_METHODS } = require('../../json_rpc/constants');
+
+async function sendGetNodeParamRequest(endpointUrl, privateKey, chainId, param) {
+  const message = {
+    timestamp: Date.now(),
+    method: JSON_RPC_METHODS.AIN_GET_NODE_PARAM,
+    param,
+  };
+  const signature = ainUtil.ecSignMessage(stringify(message), Buffer.from(privateKey, 'hex'), chainId);
+  return await axios.post(
+    `${endpointUrl}/json-rpc`,
+    {
+      method: JSON_RPC_METHODS.AIN_GET_NODE_PARAM,
+      params: {
+        protoVer: BlockchainConsts.CURRENT_PROTOCOL_VERSION,
+        message,
+        signature,
+      },
+      jsonrpc: '2.0',
+      id: 0
+    }
+  ).then(function(resp) {
+    return _.get(resp, 'data.result.result');
+  });
+}
+
+async function getNodeParam(endpointUrl, chainId, type, keystoreFilePath, param) {
+  const privateKey = await getAccountPrivateKey(type, keystoreFilePath);
+  const res = await sendGetNodeParamRequest(endpointUrl, privateKey, chainId, param);
+  console.log('Result:', res);
+}
+
+async function processArguments() {
+  if (process.argv.length !== 6 && process.argv.length !== 7) {
+    usage();
+  }
+  const endpointUrl = process.argv[2];
+  const chainId = Number(process.argv[3]);
+  const accountType = process.argv[4];
+  let keystoreFilePath = null;
+  let param = null;
+  if (accountType === 'keystore') {
+    keystoreFilePath = process.argv[5];
+    param = process.argv[6];
+  } else {
+    param = process.argv[5];
+  }
+  await getNodeParam(endpointUrl, chainId, accountType, keystoreFilePath, param);
+}
+
+function usage() {
+  console.log('\nUsage:\n  node getNodeParam.js <NODE_ENDPOINT> <CHAIN_ID> <ACCOUNT_TYPE> [<KEYSTORE_FILE_PATH>] <PARAM>\n');
+  console.log('\nExamples:');
+  console.log('node tools/api-access/getNodeParam.js http://localhost:8081 0 private_key DEV_CLIENT_API_IP_WHITELIST');
+  console.log('node tools/api-access/getNodeParam.js http://localhost:8081 0 mnemonic P2P_MESSAGE_TIMEOUT_MS');
+  console.log('node tools/api-access/getNodeParam.js http://localhost:8081 0 keystore /path/to/keystore/file SYNC_MODE');
+  process.exit(0);
+}
+
+processArguments();
