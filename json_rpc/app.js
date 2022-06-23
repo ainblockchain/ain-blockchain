b4dd095b005a5eeea8c224af89e54a568b4f88d6
Dongil Seo <seo@comcom.ai>
2022-06-23 20:21:16 +0900

    Fix result format of ain_validateAppName() json rpc API

--- json_rpc/app.js
+++ json_rpc/app.js
@@ -9,10 +9,10 @@ module.exports = function getAppApis(node) {
   return {
     [JSON_RPC_METHODS.AIN_VALIDATE_APP_NAME]: function(args, done) {
       const beginTime = Date.now();
-      const result = node.validateAppName(args.app_name);
+      const retVal = node.validateAppName(args.app_name);
       const latency = Date.now() - beginTime;
       trafficStatsManager.addEvent(TrafficEventTypes.JSON_RPC_GET, latency);
-      done(null, JsonRpcUtil.addProtocolVersion({ result }));
+      done(null, JsonRpcUtil.addProtocolVersion(retVal));
     },
   };
 };
