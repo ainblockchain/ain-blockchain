aea805ca4dea15dc01b922cc1558ae50cda7f008
Dongil Seo <seo@comcom.ai>
2022-11-23 12:57:40 +0900

    Add allow_non_negative_transfer_value_only bandage file

---
+++ db/bandage-files/allow_non_negative_transfer_value_only.js
@@ -0,0 +1,17 @@
+module.exports = {
+  data: [
+    {
+      path: ['rules', 'transfer', '$from', '$to', '$key', 'value' ],
+      value: {
+        '.rule': {
+          "write": "(auth.addr === $from || auth.fid === '_stake' || auth.fid === '_unstake' || auth.fid === '_pay' || auth.fid === '_claim' || auth.fid === '_hold' || auth.fid === '_release' || auth.fid === '_collectFee' || auth.fid === '_claimReward' || auth.fid === '_openCheckout' || auth.fid === '_closeCheckout' || auth.fid === '_closeCheckin') && !getValue('transfer/' + $from + '/' + $to + '/' + $key) && (util.isServAcntName($from) || util.isCksumAddr($from)) && (util.isServAcntName($to) || util.isCksumAddr($to)) && $from !== $to && util.isNumber(newData) && newData > 0 && util.getBalance($from, getValue) >= newData"
+        }
+      },
+      prevValue: {
+        ".rule": {
+          "write": "(auth.addr === $from || auth.fid === '_stake' || auth.fid === '_unstake' || auth.fid === '_pay' || auth.fid === '_claim' || auth.fid === '_hold' || auth.fid === '_release' || auth.fid === '_collectFee' || auth.fid === '_claimReward' || auth.fid === '_openCheckout' || auth.fid === '_closeCheckout' || auth.fid === '_closeCheckin') && !getValue('transfer/' + $from + '/' + $to + '/' + $key) && (util.isServAcntName($from) || util.isCksumAddr($from)) && (util.isServAcntName($to) || util.isCksumAddr($to)) && $from !== $to && util.isNumber(newData) && util.getBalance($from, getValue) >= newData"
+        }
+      }
+    }
+  ]
+};
