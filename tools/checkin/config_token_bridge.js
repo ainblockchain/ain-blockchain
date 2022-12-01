825c7d761c57fbf9f357e7cc37666f8c218bcb22
liayoo <lia@comcom.ai>
2022-12-01 19:06:04 +0900

    Add tool script configs for token bridge env.

---
+++ tools/checkin/config_token_bridge.js
@@ -0,0 +1,14 @@
+module.exports = {
+  endpointUrl: "http://localhost:8081",
+  ainErc20Address: "0x5d73565cbf3d6dE9610CD8a16a062B1b9dCff97a",
+  userAddr: "0x09A0d53FDf1c36A131938eb379b98910e55EEfe1",
+  userPrivateKey: "ee0b1315d446e5318eb6eb4e9d071cd12ef42d2956d546f9acbdc3b75c469640",
+  // // Ganache Eth address
+  senderAddr: "0x6d01607E0b9754C40D56ccb8B7ED224b12858317",
+  senderPrivateKey: 'REDACTED',
+  // AIN Native token pool (Local)
+  tokenPoolAddr: "0x20ADd3d38405ebA6338CB9e57a0510DEB8f8e000",
+  tokenPoolPrivateKey: "REDACTED",
+  checkinId: Date.now(),
+  tokenAmount: 10000
+};
