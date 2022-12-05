4be3402ea113d22a7424f0d077a56991f5b87524
liayoo <lia@comcom.ai>
2022-12-05 17:04:53 +0900

    Add blockchain params, genesis block, script for token bridge testing.

--- tools/checkin/config_token_bridge_local.js
+++ tools/checkin/config_token_bridge_local.js
@@ -3,11 +3,12 @@ module.exports = {
   ainErc20Address: "0x5d73565cbf3d6dE9610CD8a16a062B1b9dCff97a",
   userAddr: "0x09A0d53FDf1c36A131938eb379b98910e55EEfe1",
   userPrivateKey: "ee0b1315d446e5318eb6eb4e9d071cd12ef42d2956d546f9acbdc3b75c469640",
-  // Ganache Eth address
-  recipientAddr: "0x6d01607E0b9754C40D56ccb8B7ED224b12858317",
+  // // Ganache Eth address
+  senderAddr: "0x6d01607E0b9754C40D56ccb8B7ED224b12858317",
+  senderPrivateKey: 'b178cf12d4126ea1db48ca32e3ce6743580ca6646391996032fc76652d699972',
   // AIN Native token pool (Local)
   tokenPoolAddr: "0x20ADd3d38405ebA6338CB9e57a0510DEB8f8e000",
-  tokenPoolPrivateKey: "REDACTED",
-  checkoutId: 1669889444877,
+  tokenPoolPrivateKey: "d42f73de4ee706a4891dad643e0a65c0677020dbc2425f585442d0de2c742a44",
+  checkinId: 1669889353361,
   tokenAmount: 10000
 };
--- tools/checkout/config_token_bridge_local.js
+++ tools/checkout/config_token_bridge_local.js
@@ -3,12 +3,11 @@ module.exports = {
   ainErc20Address: "0x5d73565cbf3d6dE9610CD8a16a062B1b9dCff97a",
   userAddr: "0x09A0d53FDf1c36A131938eb379b98910e55EEfe1",
   userPrivateKey: "ee0b1315d446e5318eb6eb4e9d071cd12ef42d2956d546f9acbdc3b75c469640",
-  // // Ganache Eth address
-  senderAddr: "0x6d01607E0b9754C40D56ccb8B7ED224b12858317",
-  senderPrivateKey: 'REDACTED',
+  // Ganache Eth address
+  recipientAddr: "0x6d01607E0b9754C40D56ccb8B7ED224b12858317",
   // AIN Native token pool (Local)
   tokenPoolAddr: "0x20ADd3d38405ebA6338CB9e57a0510DEB8f8e000",
-  tokenPoolPrivateKey: "REDACTED",
-  checkinId: 1669889353361,
+  tokenPoolPrivateKey: "d42f73de4ee706a4891dad643e0a65c0677020dbc2425f585442d0de2c742a44",
+  checkoutId: 1669889444877,
   tokenAmount: 10000
 };
