518dd615a2856bb9f6299ca1e2199d28f7841e99
Dongil Seo <seo@comcom.ai>
2023-04-27 15:21:44 +0900

    Add sendAddToDevelopersUserWhitelistTx.js developers tool

---
+++ tools/developers/config_gcp.js
@@ -0,0 +1,6 @@
+module.exports = {
+  endpointUrl: "https://staging-api.ainetwork.ai",
+  developerAddr: "0x09A0d53FDf1c36A131938eb379b98910e55EEfe1",
+  serviceOwnerAddr: "0xAAAf6f50A0304F12119D218b94bea8082642515B",
+  serviceOwnerPrivateKey: "a2b5848760d81afe205884284716f90356ad82be5ab77b8130980bdb0b7ba2ba",
+};
