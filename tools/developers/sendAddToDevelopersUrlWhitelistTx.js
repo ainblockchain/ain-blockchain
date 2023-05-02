6ed1989e6916fb81c4ed0fb6666d1bd17bdc18bf
Dongil Seo <seo@comcom.ai>
2023-05-02 21:13:28 +0900

    Add chain id option to developer tools

--- tools/developers/sendAddToDevelopersUrlWhitelistTx.js
+++ tools/developers/sendAddToDevelopersUrlWhitelistTx.js
@@ -1,11 +1,9 @@
 // A tool to register a URL to the developers URL whitelist (i.e., REST function URL whitelist).
 // This should be executed with developer's keystore files.
 // This can be tested with the tool scripts under tools/chatbot.
-const path = require('path');
 const CommonUtil = require('../../common/common-util');
 const PathUtil = require('../../common/path-util');
 const { keystoreToAccount, signAndSendTx, confirmTransaction } = require('../util');
-let config = {};
 
 function buildTxBody(timestamp, address, urlKey, urlValue) {
   const urlPath = CommonUtil.appendPath(
@@ -22,7 +20,7 @@ function buildTxBody(timestamp, address, urlKey, urlValue) {
   };
 }
 
-async function sendTransaction(urlKey, urlValue, keystoreAccount) {
+async function sendTransaction(endpointUrl, chainId, urlKey, urlValue, keystoreAccount) {
   console.log('\n*** sendTransaction():');
   const timestamp = Date.now();
 
@@ -33,38 +31,41 @@ async function sendTransaction(urlKey, urlValue, keystoreAccount) {
   const txBody = buildTxBody(timestamp, keystoreAccount.address, urlKey, urlValue);
   console.log(`txBody: ${JSON.stringify(txBody, null, 2)}`);
 
-  const txInfo = await signAndSendTx(config.endpointUrl, txBody, keystoreAccount.private_key);
+  const txInfo = await signAndSendTx(endpointUrl, txBody, keystoreAccount.private_key, chainId);
   console.log(`txInfo: ${JSON.stringify(txInfo, null, 2)}`);
   if (!txInfo.success) {
     console.log(`Transaction failed.`);
     process.exit(0);
   }
-  await confirmTransaction(config.endpointUrl, timestamp, txInfo.txHash);
+  await confirmTransaction(endpointUrl, timestamp, txInfo.txHash);
 }
 
 async function processArguments() {
-  if (process.argv.length !== 6) {
+  if (process.argv.length !== 7) {
     usage();
   }
-  config = require(path.resolve(__dirname, process.argv[2]));
-  const urlKey = process.argv[3];
+  const endpointUrl = process.argv[2];
+  const chainId = Number(process.argv[3]);
+  const urlKey = process.argv[4];
   if (!CommonUtil.isString(urlKey) || urlKey.length == 0) {
     console.log(`The URL key is NOT a valid one: ${urlKey}`);
     process.exit(0);
   }
-  const urlValue = process.argv[4];
+  const urlValue = process.argv[5];
   if (!CommonUtil.isValidUrlWhitelistItem(urlValue)) {
     console.log(`The URL value is NOT a valid whitelist item: ${urlValue}`);
     process.exit(0);
   }
-  const keystoreAccount = await keystoreToAccount(process.argv[5]);
+  const keystoreAccount = await keystoreToAccount(process.argv[6]);
   console.log(`\nKeystore account address: ${keystoreAccount.address}\n`);
-  await sendTransaction(urlKey, urlValue, keystoreAccount);
+  await sendTransaction(endpointUrl, chainId, urlKey, urlValue, keystoreAccount);
 }
 
 function usage() {
-  console.log('\nUsage: node sendAddToDevelopersUrlWhitelistTx.js <Config File> <URL Key> <URL Value> <Developer Keystore Filepath>\n');
-  console.log('Example: node sendAddToDevelopersUrlWhitelistTx.js config_local.js 0 http://localhost:8000 keystore_developer.json\n');
+  console.log('\nUsage: node sendAddToDevelopersUrlWhitelistTx.js <Endpoint Url> <Chain Id> <URL Key> <URL Value> <Developer Keystore Filepath>\n');
+  console.log('Example: node sendAddToDevelopersUrlWhitelistTx.js http://localhost:8081 0 0 http://localhost:8000 keystore_developer.json');
+  console.log('Example: node sendAddToDevelopersUrlWhitelistTx.js https://staging-api.ainetwork.ai 0 0 https://mydomain.com keystore_developer.json');
+  console.log('Example: node sendAddToDevelopersUrlWhitelistTx.js https://mainnet-api.ainetwork.ai 1 0 https://mydomain.com keystore_developer.json\n');
   process.exit(0)
 }
 
--- tools/developers/sendRemoveFromDevelopersUserWhitelistTx.js
+++ tools/developers/sendRemoveFromDevelopersUserWhitelistTx.js
@@ -1,11 +1,9 @@
 // A tool to deregister an address from the developers user whitelist.
 // This should be executed with blockchain owner's keystore files.
 // This can be tested with the tool scripts under tools/chatbot.
-const path = require('path');
 const CommonUtil = require('../../common/common-util');
 const PathUtil = require('../../common/path-util');
 const { keystoreToAccount, signAndSendTx, confirmTransaction } = require('../util');
-let config = {};
 
 function buildTxBody(timestamp, address) {
   const userPath = PathUtil.getDevelopersRestFunctionsUserWhitelistUserPath(address);
@@ -21,40 +19,43 @@ function buildTxBody(timestamp, address) {
   };
 }
 
-async function sendTransaction(developerAddr, keystoreAccount) {
+async function sendTransaction(endpointUrl, chainId, developerAddr, keystoreAccount) {
   console.log('\n*** sendTransaction():');
   const timestamp = Date.now();
 
   const txBody = buildTxBody(timestamp, developerAddr);
   console.log(`txBody: ${JSON.stringify(txBody, null, 2)}`);
 
-  const txInfo = await signAndSendTx(config.endpointUrl, txBody, keystoreAccount.private_key);
+  const txInfo = await signAndSendTx(endpointUrl, txBody, keystoreAccount.private_key, chainId);
   console.log(`txInfo: ${JSON.stringify(txInfo, null, 2)}`);
   if (!txInfo.success) {
     console.log(`Transaction failed.`);
     process.exit(0);
   }
-  await confirmTransaction(config.endpointUrl, timestamp, txInfo.txHash);
+  await confirmTransaction(endpointUrl, timestamp, txInfo.txHash);
 }
 
 async function processArguments() {
-  if (process.argv.length !== 5) {
+  if (process.argv.length !== 6) {
     usage();
   }
-  config = require(path.resolve(__dirname, process.argv[2]));
-  const developerAddr = process.argv[3];
+  const endpointUrl = process.argv[2];
+  const chainId = Number(process.argv[3]);
+  const developerAddr = process.argv[4];
   if (!CommonUtil.isCksumAddr(developerAddr)) {
     console.log(`The developer address is NOT a checksum address: ${developerAddr}`);
     process.exit(0);
   }
-  const keystoreAccount = await keystoreToAccount(process.argv[4]);
+  const keystoreAccount = await keystoreToAccount(process.argv[5]);
   console.log(`\nKeystore account address: ${keystoreAccount.address}\n`);
-  await sendTransaction(developerAddr, keystoreAccount);
+  await sendTransaction(endpointUrl, chainId, developerAddr, keystoreAccount);
 }
 
 function usage() {
-  console.log('\nUsage: node sendRemoveFromDevelopersUserWhitelistTx.js <Config File> <Developer Address> <Blockchain Owner Keystore Filepath>\n');
-  console.log('Example: node sendRemoveFromDevelopersUserWhitelistTx.js config_local.js 0x08Aed7AF9354435c38d52143EE50ac839D20696b keystore_blockchain_owner.json\n');
+  console.log('\nUsage: node sendRemoveFromDevelopersUserWhitelistTx.js <Endpoint Url> <Developer Address> <Blockchain Owner Keystore Filepath>\n');
+  console.log('Example: node sendRemoveFromDevelopersUserWhitelistTx.js http://localhost:8081 0 0x08Aed7AF9354435c38d52143EE50ac839D20696b keystore_blockchain_owner.json');
+  console.log('Example: node sendRemoveFromDevelopersUserWhitelistTx.js https://staging-api.ainetwork.ai 0 0x08Aed7AF9354435c38d52143EE50ac839D20696b keystore_blockchain_owner.json');
+  console.log('Example: node sendRemoveFromDevelopersUserWhitelistTx.js https://mainnet-api.ainetwork.ai 1 0x08Aed7AF9354435c38d52143EE50ac839D20696b keystore_blockchain_owner.json\n');
   process.exit(0)
 }
 
