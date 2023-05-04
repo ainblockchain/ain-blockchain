73deab7da5527221ef3e33e15a83182cc049f297
Dongil Seo <seo@comcom.ai>
2023-05-04 21:30:59 +0900

    Add other key options to developers tools

--- tools/developers/sendRemoveFromDevelopersUserWhitelistTx.js
+++ tools/developers/sendRemoveFromDevelopersUserWhitelistTx.js
@@ -3,7 +3,7 @@
 // This can be tested with the tool scripts under tools/chatbot.
 const CommonUtil = require('../../common/common-util');
 const PathUtil = require('../../common/path-util');
-const { keystoreToAccount, signAndSendTx, confirmTransaction } = require('../util');
+const { getAccountPrivateKey, signAndSendTx, confirmTransaction } = require('../util');
 
 function buildTxBody(timestamp, address) {
   const userPath = PathUtil.getDevelopersRestFunctionsUserWhitelistUserPath(address);
@@ -19,14 +19,14 @@ function buildTxBody(timestamp, address) {
   };
 }
 
-async function sendTransaction(endpointUrl, chainId, developerAddr, keystoreAccount) {
+async function sendTransaction(endpointUrl, chainId, developerAddr, privateKey) {
   console.log('\n*** sendTransaction():');
   const timestamp = Date.now();
 
   const txBody = buildTxBody(timestamp, developerAddr);
   console.log(`txBody: ${JSON.stringify(txBody, null, 2)}`);
 
-  const txInfo = await signAndSendTx(endpointUrl, txBody, keystoreAccount.private_key, chainId);
+  const txInfo = await signAndSendTx(endpointUrl, txBody, privateKey, chainId);
   console.log(`txInfo: ${JSON.stringify(txInfo, null, 2)}`);
   if (!txInfo.success) {
     console.log(`Transaction failed.`);
@@ -35,8 +35,13 @@ async function sendTransaction(endpointUrl, chainId, developerAddr, keystoreAcco
   await confirmTransaction(endpointUrl, timestamp, txInfo.txHash);
 }
 
+async function sendRemoveFromDevelopersUserWhitelistTx(endpointUrl, chainId, developerAddr, accountType, keystoreFilepath) {
+  const privateKey = await getAccountPrivateKey(accountType, keystoreFilepath);
+  await sendTransaction(endpointUrl, chainId, developerAddr, privateKey);
+}
+
 async function processArguments() {
-  if (process.argv.length !== 6) {
+  if (process.argv.length !== 6 && process.argv.length !== 7) {
     usage();
   }
   const endpointUrl = process.argv[2];
@@ -46,16 +51,23 @@ async function processArguments() {
     console.log(`The developer address is NOT a checksum address: ${developerAddr}`);
     process.exit(0);
   }
-  const keystoreAccount = await keystoreToAccount(process.argv[5]);
-  console.log(`\nKeystore account address: ${keystoreAccount.address}\n`);
-  await sendTransaction(endpointUrl, chainId, developerAddr, keystoreAccount);
+  const accountType = process.argv[5];
+  const keystoreFilepath = (accountType === 'keystore') ? process.argv[6] : null;
+  if (accountType === 'keystore' && !keystoreFilepath) {
+    console.error('Please specify keystore filepath.');
+    usage();
+  }
+  await sendRemoveFromDevelopersUserWhitelistTx(endpointUrl, chainId, developerAddr, accountType, keystoreFilepath);
 }
 
 function usage() {
-  console.log('\nUsage: node sendRemoveFromDevelopersUserWhitelistTx.js <Endpoint Url> <Developer Address> <Blockchain Owner Keystore Filepath>\n');
-  console.log('Example: node sendRemoveFromDevelopersUserWhitelistTx.js http://localhost:8081 0 0x08Aed7AF9354435c38d52143EE50ac839D20696b keystore_blockchain_owner.json');
-  console.log('Example: node sendRemoveFromDevelopersUserWhitelistTx.js https://staging-api.ainetwork.ai 0 0x08Aed7AF9354435c38d52143EE50ac839D20696b keystore_blockchain_owner.json');
-  console.log('Example: node sendRemoveFromDevelopersUserWhitelistTx.js https://mainnet-api.ainetwork.ai 1 0x08Aed7AF9354435c38d52143EE50ac839D20696b keystore_blockchain_owner.json\n');
+  console.log('\nUsage: node sendRemoveFromDevelopersUserWhitelistTx.js <Endpoint Url> <Developer Address> <Account Type> [<Keystore Filepath>]\n');
+  console.log('Example: node sendRemoveFromDevelopersUserWhitelistTx.js http://localhost:8081 0 0x08Aed7AF9354435c38d52143EE50ac839D20696b private_key');
+  console.log('Example: node sendRemoveFromDevelopersUserWhitelistTx.js http://localhost:8081 0 0x08Aed7AF9354435c38d52143EE50ac839D20696b mnemonic');
+  console.log('Example: node sendRemoveFromDevelopersUserWhitelistTx.js http://localhost:8081 0 0x08Aed7AF9354435c38d52143EE50ac839D20696b keystore keystore_blockchain_owner.json');
+  console.log('Example: node sendRemoveFromDevelopersUserWhitelistTx.js https://staging-api.ainetwork.ai 0 0x08Aed7AF9354435c38d52143EE50ac839D20696b keystore keystore_blockchain_owner.json');
+  console.log('Example: node sendRemoveFromDevelopersUserWhitelistTx.js https://testnet-api.ainetwork.ai 0 0x08Aed7AF9354435c38d52143EE50ac839D20696b keystore keystore_blockchain_owner.json');
+  console.log('Example: node sendRemoveFromDevelopersUserWhitelistTx.js https://mainnet-api.ainetwork.ai 1 0x08Aed7AF9354435c38d52143EE50ac839D20696b keystore keystore_blockchain_owner.json\n');
   process.exit(0)
 }
 
