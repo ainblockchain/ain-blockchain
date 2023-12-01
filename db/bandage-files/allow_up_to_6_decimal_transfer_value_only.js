e13a525280aec2bec205c28d58e669ab009cdff4
Dongil Seo <seo@comcom.ai>
2023-12-01 16:58:09 +0900

    Add allow_up_to_6_decimal_transfer_value_only timer flag and bandage file

---
+++ db/bandage-files/allow_up_to_6_decimal_transfer_value_only.js
@@ -0,0 +1,18 @@
+module.exports = {
+  data: [
+    {
+      path: ['rules', 'transfer', '$from', '$to', '$key', 'value' ],
+      value: {
+        '.rule': {
+          "write": "(auth.addr === $from || auth.fid === '_stake' || auth.fid === '_unstake' || auth.fid === '_pay' || auth.fid === '_claim' || auth.fid === '_hold' || auth.fid === '_release' || auth.fid === '_collectFee' || auth.fid === '_claimReward' || auth.fid === '_openCheckout' || auth.fid === '_closeCheckout' || auth.fid === '_closeCheckin') && !getValue('transfer/' + $from + '/' + $to + '/' + $key) && (util.isServAcntName($from, blockNumber) || util.isCksumAddr($from)) && (util.isServAcntName($to, blockNumber) || util.isCksumAddr($to)) && $from !== $to && util.isNumber(newData) && newData > 0 && util.countDecimals(newData) <= 6 && util.getBalance($from, getValue) >= newData"
+        }
+      },
+      // From allow_non_negative_transfer_value_only.js bandage file.
+      prevValue: {
+        ".rule": {
+          "write": "(auth.addr === $from || auth.fid === '_stake' || auth.fid === '_unstake' || auth.fid === '_pay' || auth.fid === '_claim' || auth.fid === '_hold' || auth.fid === '_release' || auth.fid === '_collectFee' || auth.fid === '_claimReward' || auth.fid === '_openCheckout' || auth.fid === '_closeCheckout' || auth.fid === '_closeCheckin') && !getValue('transfer/' + $from + '/' + $to + '/' + $key) && (util.isServAcntName($from, blockNumber) || util.isCksumAddr($from)) && (util.isServAcntName($to, blockNumber) || util.isCksumAddr($to)) && $from !== $to && util.isNumber(newData) && newData > 0 && util.getBalance($from, getValue) >= newData"
+        }
+      }
+    }
+  ]
+};
